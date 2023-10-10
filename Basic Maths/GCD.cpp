e0e1670e24b1093ed6779748c4e221336a4daf1a
Prakhar Maheshwari <prakhar2115430@akgec.ac.in>
2023-10-10 17:27:57 +0530

    gcd

---
+++ Basic Maths/GCD.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+	public:
+    int gcd(int A, int B) 
+	{ 
+	    while(A>0 && B>0){
+	        if(A>B){
+	            A=A-B;
+	        }else{
+	            B=B-A;
+	        }
+	    }
+	    return A==0 ? B : A ;
+	      
+	} 
+};
+
+// time complexity: O(log(min(A,B)))
