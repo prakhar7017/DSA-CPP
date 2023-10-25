79318fafeab9daee9a442ec9af95d84ad7911a37
Prakhar Maheshwari <prakhar2115430@akgec.ac.in>
2023-10-25 21:34:31 +0530

    maximum points

---
+++ Arrays/1423. Maximum Points You Can Obtain from Cards.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxScore(vector<int>& cardPoints, int k) {
+        int ans=0;
+        for(int i=0;i<k;i++){
+            ans+=cardPoints[i];
+        }
+        int sum=ans;
+        for(int i=k-1;i>=0;i--){
+            sum-=cardPoints[i];
+            sum+=cardPoints[cardPoints.size()-k+i];
+            ans=max(ans,sum);
+        }
+        return ans;
+    }
+};
+
+// time complexity : O(n) n is the size of the array 
+// sliding window approach
