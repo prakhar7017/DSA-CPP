ffc978dffc838adb73dd8f85e0db7f57c41ef2c5
Prakhar Maheshwari <prakhar2115430@akgec.ac.in>
2023-10-27 22:59:39 +0530

    majority morre voting alog

--- Arrays/169. Majority Element.cpp
+++ Arrays/169. Majority Element.cpp
@@ -16,4 +16,31 @@ public:
     }
 };
 
-time complexity : O(nlogn) n is the size of the array
+// time complexity : O(nlogn) n is the size of the array
+
+
+// Morre Voting Algorithm : https://www.youtube.com/watch?v=nP_ns3uSh80
+
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n=nums.size();
+        int count=0;
+        int el;
+        for(int i=0;i<n;i++){
+            if(count==0){
+                count++;
+                el=nums[i];
+            }else if(nums[i]==el){
+                count++;
+            }else{
+                count--;
+            }
+        }
+        return el;
+    }
+};
+
+// time complexity : O(n) n is the size of the array
+
