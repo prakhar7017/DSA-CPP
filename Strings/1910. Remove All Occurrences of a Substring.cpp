564ba46c54c675d1866fcef405c510b886c5bef5
Prakhar Maheshwari <prakhar2115430@akgec.ac.in>
2023-10-29 16:25:38 +0530

    remove occurance recurisve

--- Strings/1910. Remove All Occurrences of a Substring.cpp
+++ Strings/1910. Remove All Occurrences of a Substring.cpp
@@ -9,3 +9,29 @@ public:
 };
 
 // time complxity : O(n*m) n is the size of the string and m is the size of the part string
+
+// Recursive Solution 
+
+
+class Solution {
+    private:
+    void remOccurance(string &s,string &part){
+        int found=s.find(part);
+        if(found!=string::npos){
+            string left=s.substr(0,found);
+            string right=s.substr(found+part.size(),s.size());
+            s=left+right;
+            remOccurance(s,part);
+        }else{
+            //base case
+            return;
+        }
+    }
+public:
+    string removeOccurrences(string s, string part) {
+        remOccurance(s,part);
+        return s;
+    }
+};
+
+// time complexity : O(n^2)
