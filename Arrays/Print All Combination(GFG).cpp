4cc58cb0e8536a115b4614140f2be59d6d5c76b7
Prakhar Maheshwari <prakhar2115430@akgec.ac.in>
2023-10-29 17:27:08 +0530

    handle duplucates

--- Arrays/Print All Combination(GFG).cpp
+++ Arrays/Print All Combination(GFG).cpp
@@ -50,3 +50,51 @@ output->
 2 3 5 
 2 4 5 
 3 4 5 
+
+// for handeling dublicates 
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void findCombinations(vector<int>& nums, vector<int> data, int index, int r, vector<vector<int>>& ans) {
+
+  //base case
+  if (data.size() == r) {
+    ans.push_back(data);
+    return;
+  }
+  
+  if (index >= nums.size()) {
+    return;
+  }
+  
+  //processing // recursive calls
+  data.push_back(nums[index]);
+  findCombinations(nums, data, index + 1, r, ans);
+  
+  while (index < nums.size() - 1 && nums[index] == nums[index + 1]) {
+    index++;
+  }
+  
+  data.pop_back();
+  findCombinations(nums, data, index + 1, r, ans);
+}
+
+int main() {
+  vector<vector<int>> ans; 
+  vector<int> nums = {1, 2, 1, 3, 1};
+  sort(nums.begin(),nums.end());
+  int r = 3;
+  vector<int> data;
+  findCombinations(nums, data, 0, r, ans);
+
+  for (auto i : ans) {
+    for (int num : i) {
+      cout << num << " ";
+    }
+    cout << endl;
+  }
+
+  return 0;
+}
