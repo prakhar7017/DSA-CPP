6d52d2c45610c4cb9adf3dc0b4b9bbaac103bb26
Prakhar Maheshwari <prakhar2115430@akgec.ac.in>
2023-11-04 02:19:59 +0530

    in place merge sort

---
+++ Sorting/MergeSort(In-Place).cpp
@@ -0,0 +1,45 @@
+class Solution {
+    private:
+    // using gap method to merge two sorted arrays in O(1) space
+    void merge(vector<int>& arr, int s, int e) {
+        int totalLen=e-s+1;
+        int gap=(totalLen/2)+(totalLen%2);
+        while(gap>0){
+            int i=s;
+            int j=s+gap;
+            while(j<=e){
+                if(arr[i]>arr[j]){
+                    swap(arr[i],arr[j]);
+                }
+                ++i;
+                ++j;
+            }
+            gap= gap<=1 ? 0 : (gap/2)+(gap%2);
+        }
+    }
+
+    void mergeSort(vector<int>& arr, int s, int e) {
+
+        if (s >= e) {
+             return;
+        }
+
+        int mid = s + (e - s) / 2;
+
+        mergeSort(arr, s, mid);
+
+        mergeSort(arr, mid + 1, e);
+
+        merge(arr, s, e);
+
+    }
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        int s=0;
+        int e=nums.size()-1;
+        mergeSort(nums,s,e);
+        return nums;
+    }
+};
+
+time complexity: O(nlogn)
