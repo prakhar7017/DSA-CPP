dafc94a0da3494a2bd03ea5ee0a367647ad3086b
Prakhar Maheshwari <prakhar2115430@akgec.ac.in>
2023-12-03 22:48:46 +0530

    next&prev smaller elenebt

---
+++ Stack/next&prev smaller elemet.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+using namespace std;
+
+
+void prevSmallerElement(vector<int>&arr,vector<int>&ans){
+  stack<int>st;
+  st.push(-1); // edge case for last element
+
+  for(int i=0;i<arr.size();i++){
+    int curr=arr[i];
+    while(!st.empty() && st.top()>=curr){
+      st.pop();
+    }
+    ans[i]=st.top();
+
+    st.push(curr);
+  }
+}
+void nextSmallerElement(vector<int>&arr,vector<int>&ans){
+  stack<int>st;
+  st.push(-1); // edge case for last element
+
+  for(int i=arr.size()-1;i>=0;i--){
+    int curr=arr[i];
+    while(!st.empty() && st.top()>=curr){
+      st.pop();
+    }
+    ans[i]=st.top();
+
+    st.push(curr);
+  }
+}
+
+int main() {
+  vector<int>arr={8,4,6,2,3};
+  vector<int>ans(5);
+  prevSmallerElement(arr,ans);
+  for(auto it:ans){
+    cout<<it<<" ";
+  }
+  return 0;
+}
