13de982a6abf4b935867f7914168c033d216d70d
Prakhar Maheshwari <prakhar2115430@akgec.ac.in>
2023-12-08 03:32:11 +0530

    removal of adjacent using stack

--- Strings/1047. Remove All Adjacent Duplicates In String.cpp
+++ Strings/1047. Remove All Adjacent Duplicates In String.cpp
@@ -17,3 +17,29 @@ public:
 
 // time complexity:O(n)
 
+// using stack 
+
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        string ans="";
+        stack<char>st;
+        for(auto ch:s){
+            if(!st.empty() && st.top()==ch){
+                st.pop();
+            }else{
+                st.push(ch);
+            }
+        }
+        while(!st.empty()){
+            char ch=st.top();
+            st.pop();
+            ans+=ch;
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
+
+// time compelxity: O(n)
+
