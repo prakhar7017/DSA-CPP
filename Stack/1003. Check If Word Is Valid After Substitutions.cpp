e02024ddaf7a036ad25e1701472da4faf740609d
Prakhar Maheshwari <prakhar2115430@akgec.ac.in>
2023-12-09 17:21:08 +0530

    Decode String

--- Stack/1003. Check If Word Is Valid After Substitutions.cpp
+++ Stack/1003. Check If Word Is Valid After Substitutions.cpp
@@ -34,3 +34,4 @@ public:
 };
 
 // time complexity: O(n)
+
---
+++ Stack/394. Decode String.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    string decodeString(string s) {
+        stack<string>st;
+        for(auto ch:s){
+            if(ch==']'){
+                string stringToRepeat="";
+                while(!st.empty() && st.top()!="["){
+                    string top=st.top();
+                    stringToRepeat+=top;
+                    st.pop();
+                }
+                st.pop(); // it remove "]"
+                string numericTimes="";
+                while(!st.empty() && isdigit(st.top()[0])){
+                    numericTimes+=st.top();
+                    st.pop();
+                }
+                reverse(numericTimes.begin(),numericTimes.end());
+                int n=stoi(numericTimes);
+                string finaldecoding="";
+                while(n!=0){
+                    finaldecoding+=stringToRepeat;
+                    n--;
+                }
+                st.push(finaldecoding); 
+            }else{
+                string temp(1,ch);
+                st.push(temp);
+            }
+        }
+        string ans;
+        while(!st.empty()){
+            ans+=st.top();
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
