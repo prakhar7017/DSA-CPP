303ac60761df6bf11b7d7950d39d3f2b9bfc644e
Prakhar Maheshwari <prakhar2115430@akgec.ac.in>
2023-12-12 18:07:51 +0530

    circular queue

--- Queue/Basics/circular queue.cpp
+++ Queue/Basics/circular queue.cpp
@@ -1,23 +1,26 @@
 #include <iostream>
 using namespace std;
 
-class Queue{
+class CQueue{
   int* arr;
   int size;
   int front;
   int rear;
 
   public:
-    Queue(int s):size(s),arr(new int[s]),front(-1),rear(-1){};
+    CQueue(int s):size(s),arr(new int[s]),front(-1),rear(-1){};
 
     void push(int data){
       // overflow
-      if(rear==size-1){
+      if((front==0 && rear==size-1) || ( front-rear==1 )){
         cout<<"Overflow "<<endl;
         return; 
       }else if(front==-1 && rear==-1){ // first element
         rear++;
         front++;
+        arr[rear]=data;
+      }else if(rear==size-1 && front!=0){
+        rear=0;
         arr[rear]=data;
       }else{ // noraml case
         rear++;
@@ -34,6 +37,9 @@ class Queue{
         arr[front]=-1;
         front=-1;
         rear=-1;
+      }else if(front==size-1){
+        arr[front]=-1;
+        front=0;
       }else{ // normal pop
         arr[front]=-1;
         front++;
@@ -50,10 +56,10 @@ class Queue{
     }
 
     int getSize(){
-        if(front==-1 && rear==-1){
-            return 0;
-        }
-         return rear-front+1;
+      if(front==-1 && rear==-1){
+        return 0;
+      }
+      return rear-front+1;
     }
 
     int getFront(){
@@ -76,32 +82,32 @@ class Queue{
 };
 
 int main() {
-  Queue q(5);
+  CQueue q(5);
+  q.print();
+
   q.push(10);
   q.print();
     q.push(20);
   q.print();
-    q.push(30);
+    q.push(20);
   q.print();
     q.push(40);
   q.print();
     q.push(50);
   q.print();
-    q.push(60);
-  q.print();
-
-  cout<<"front is "<<q.getFront()<<endl;
 
   q.pop();
   q.print();
-
   q.push(60);
   q.print();
-
+    q.pop();
+  q.print();
+    q.pop();
+  q.print();
+    q.pop();
+  q.print();
   q.pop();
-  q.pop();
-  cout<<"front is "<<q.getFront()<<endl;
-  q.pop();
+  q.print();
   q.pop();
   q.print();
   return 0;
