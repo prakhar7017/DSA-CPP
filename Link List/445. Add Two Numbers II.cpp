cd6906795c4d8fa8d04ff69ccf724057e21107e1
Prakhar Maheshwari <prakhar2115430@akgec.ac.in>
2023-12-13 03:37:45 +0530

    add two numbers in LL without reversing

--- Link List/445. Add Two Numbers II.cpp
+++ Link List/445. Add Two Numbers II.cpp
@@ -43,3 +43,49 @@ public:
         }
     }
 };
+
+// without reversing i.e using stack
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        if(!l1) return l2;
+        if(!l2) return l1;
+
+        stack<int>s1;
+        stack<int>s2;
+        while(l1 || l2){
+            if(l1){
+                s1.push(l1->val);
+                l1=l1->next;
+            }
+            if(l2){
+                s2.push(l2->val);
+                l2=l2->next;
+            }
+        }
+        int sum=0;
+        int carry=0;
+        ListNode* ans=new ListNode(-1);
+        while(!s1.empty() || !s2.empty()){
+            if(!s1.empty()){
+                sum+=s1.top();
+                s1.pop();
+            }
+            if(!s2.empty()){
+                sum+=s2.top();
+                s2.pop();
+            }
+            ans->val=sum%10;
+            carry=sum/10;
+            ListNode* newNode=new ListNode(carry);
+            newNode->next=ans;
+            ans=newNode;
+            sum=carry;
+        }
+        if(carry==0){
+            return ans->next;
+        }else{
+            return ans;
+        }
+    }
+};
