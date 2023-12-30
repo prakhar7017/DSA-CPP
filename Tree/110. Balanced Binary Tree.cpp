a5aa495857a8b7b23248f91c0b7ab8dc9345b85b
Prakhar Maheshwari <prakhar2115430@akgec.ac.in>
2023-12-30 15:34:46 +0530

    balanced binary tree

---
+++ Tree/110. Balanced Binary Tree.cpp
@@ -0,0 +1,39 @@
+class Solution {
+    private:
+    int height(TreeNode* root){
+        if(root==NULL){
+            return 0;
+        }
+
+        int leftHeight=height(root->left);
+        int rightHeight=height(root->right);
+
+        int height=max(leftHeight,rightHeight)+1;
+
+        return height;
+    }
+public:
+    bool isBalanced(TreeNode* root) {
+        if(root==NULL){
+            return true;
+        }
+
+        int leftHeight=height(root->left);
+        int rightHeight=height(root->right);
+
+        int diff=abs(leftHeight-rightHeight);
+
+        bool currNode=diff<=1;
+
+        bool leftAns=isBalanced(root->left);
+        bool rightAns=isBalanced(root->right);
+
+        if(currNode && leftAns && rightAns){
+            return true;
+        }else{
+            return false;
+        }
+
+    }
+};
+
