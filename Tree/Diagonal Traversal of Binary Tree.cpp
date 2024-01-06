4132af9e6da9ca362e896bfdc3e668e0fbfa5f84
Prakhar Maheshwari <prakhar2115430@akgec.ac.in>
2024-01-06 20:30:26 +0530

    diagonal traversal 2

--- Tree/Diagonal Traversal of Binary Tree.cpp
+++ Tree/Diagonal Traversal of Binary Tree.cpp
@@ -27,3 +27,26 @@ vector<int> diagonal(Node *root) {
     
     return ans;
 }
+
+
+// better solution 
+vector<int> diagonal(Node *root)
+{
+    vector<int>ans;
+    queue<Node* >q;
+    if(!root) return ans;
+    
+    q.push(root);
+    while(!q.empty()){
+        Node* temp=q.front();
+        q.pop();
+        while(temp){
+            ans.push_back(temp->data);
+            if(temp->left){
+                q.push(temp->left);
+            }
+            temp=temp->right;
+        }
+    }
+    return ans;
+}
