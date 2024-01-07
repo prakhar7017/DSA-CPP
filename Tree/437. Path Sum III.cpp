a3c1e021869d17e75833ed5de2509f108defb032
Prakhar Maheshwari <prakhar2115430@akgec.ac.in>
2024-01-07 23:06:13 +0530

    path sum III

---
+++ Tree/437. Path Sum III.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    long long  ans=0;
+    void solve(TreeNode* root,long long sum){
+        if(!root) return ;
+
+        if(sum==root->val) ++ans;
+
+        solve(root->left,sum-root->val);
+        solve(root->right,sum-root->val);
+    }
+    int pathSum(TreeNode* root, int targetSum) {
+        if(root){
+            solve(root,targetSum);
+            pathSum(root->left,targetSum);
+            pathSum(root->right,targetSum);
+        }
+
+        return ans;
+    }
+};
+
+// time complexity : O(n^2)
