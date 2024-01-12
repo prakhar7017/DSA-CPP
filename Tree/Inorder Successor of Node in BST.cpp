b491553e14c6df6a0afa917d6682991ecba454c3
Prakhar Maheshwari <prakhar2115430@akgec.ac.in>
2024-01-12 18:21:08 +0530

    inorder successor in BSt

---
+++ Tree/Inorder Successor of Node in BST.cpp
@@ -0,0 +1,14 @@
+Node* findSuccessor(Node* root, Node* p) {
+	Node* succ=0;
+	Node* curr=root;
+	while(curr){
+		if(curr->data>p->data){
+			succ=curr;
+			curr=curr->left;
+		}else{
+			curr=curr->right;
+		}
+	}
+	return succ;
+}
+
