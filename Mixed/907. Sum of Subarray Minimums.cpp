684b39b3b6b25c0f5338a6fad977f6af455b4245
Prakhar Maheshwari <prakhar2115430@akgec.ac.in>
2024-01-21 01:35:38 +0530

    sum of subarray minimums

---
+++ Mixed/907. Sum of Subarray Minimums.cpp
@@ -0,0 +1,87 @@
+// brute force
+class Solution {
+public:
+    int M = 1e9 + 7;
+    int sumSubarrayMins(std::vector<int>& arr) {
+        int n = arr.size();
+        long result = 0;
+
+        for (int i = 0; i < n; i++) {
+            int minVal = arr[i];
+
+            for (int j = i; j < n; j++) {
+                minVal = min(minVal, arr[j]);
+                result = (result + minVal) % M;
+            }
+        }
+
+        return result;
+    }
+};
+
+
+
+typedef unsigned long long ll;
+class Solution {
+public:
+    vector<int> getNSL(vector<int>& arr, int n) {
+        vector<int> result(n);
+        stack<int> st;
+        
+        for(int i = 0; i<n; i++) {
+            if(st.empty()) {
+                result[i] = -1;
+            } else {
+                while(!st.empty() && arr[st.top()] > arr[i]) //strictly less
+                    st.pop();
+                
+                result[i] = st.empty() ? -1 : st.top();
+            }
+            st.push(i);
+        }
+        
+        return result;
+    }
+    vector<int> getNSR(vector<int>& arr, int n) {
+        vector<int> result(n);
+        stack<int> st;
+        
+        for(int i = n-1; i>=0; i--) {
+            if(st.empty()) {
+                result[i] = n;
+            } else {
+                while(!st.empty() && arr[st.top()] >= arr[i]) //non-strictly less
+                    st.pop();
+                
+                result[i] = st.empty() ? n : st.top();
+            }
+            st.push(i);
+        }
+        
+        return result;
+    }
+    
+    int sumSubarrayMins(vector<int>& arr) {
+        int n = arr.size();
+        
+        vector<int> NSL = getNSL(arr, n); //Next smaller to left
+        vector<int> NSR = getNSR(arr, n); //Next smaller to right
+        
+        
+        ll sum = 0;
+        int M = 1e9+7;
+        for(int i = 0; i<n; i++) {
+            ll d1 = i - NSL[i];
+            
+            ll d2 = NSR[i] - i;
+            
+            ll total_ways_for_i_min = d1*d2;
+            ll sum_i_in_total_ways  = arr[i] * (total_ways_for_i_min);
+            
+            sum  = (sum + sum_i_in_total_ways)%M;
+        }
+        
+        return sum;
+        
+    }
+};
