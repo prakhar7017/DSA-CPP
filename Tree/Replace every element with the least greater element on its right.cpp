656ed7f2e80a97f2c2443a9038b279482134fd5a
Prakhar Maheshwari <prakhar2115430@akgec.ac.in>
2024-01-22 18:36:27 +0530

    Replace every element with the least greater element on its right

--- Tree/Replace every element with the least greater element on its right.cpp
+++ Tree/Replace every element with the least greater element on its right.cpp
@@ -1,3 +1,35 @@
+// brute force solution 
+// time complexity O(n^2)
+// space complexity O(n)
+#include <bits/stdc++.h> 
+using namespace std;
+
+int main() {
+  vector<int>arr={8,58,71,18,31,32,63,92,43,3,91,93,25,80,28};
+  int n=arr.size();
+  vector<int>ans(n,-1);
+  for(int i=0;i<n;i++){
+    int min_greater = INT_MAX;
+     for (int j = i + 1; j < n; j++) {
+      if (arr[j] > arr[i] && arr[j] < min_greater) {
+        min_greater = arr[j];
+      }
+    }
+
+    ans[i] = (min_greater == INT_MAX) ? -1 : min_greater;
+  }
+
+  cout<<endl<<"Print answer"<<endl;
+  for(int i=0;i<arr.size();i++){
+    cout<<ans[i]<<",";
+  }
+  return 0;
+}
+
+//******************************OPTIMIZED SOLUTION***********************************
+// time complexity o(n);
+// space complexity o(n);
+
 class Node
 {
 public:
