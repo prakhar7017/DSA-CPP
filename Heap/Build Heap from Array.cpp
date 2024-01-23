89db9cb10309db2c36cf9f39072305b58f962d48
Prakhar Maheshwari <prakhar2115430@akgec.ac.in>
2024-01-23 22:24:10 +0530

    Build heap from array python

--- Heap/Build Heap from Array.cpp
+++ Heap/Build Heap from Array.cpp
@@ -51,3 +51,41 @@ int main() {
 
     return 0;
 }
+
+//*********************************Python Code*********************************
+def heapify(arr, n, i):
+    left_index = 2 * i
+    right_index = 2 * i + 1
+
+    largest = i
+    if left_index <= n and arr[largest] < arr[left_index]:
+        largest = left_index
+    if right_index <= n and arr[largest] < arr[right_index]:
+        largest = right_index
+
+    if i != largest:
+        arr[i], arr[largest] = arr[largest], arr[i]
+        i = largest
+        heapify(arr, n, i)
+
+def buildHeap(arr, n):
+    pos = n // 2
+    for i in range(pos, 0, -1):
+        heapify(arr, n, i)
+
+def main():
+    arr = [-1, 5, 10, 15, 20, 25, 12]
+    n = len(arr)
+
+    print("Before Heapify")
+    for i in range(1, n):
+        print(arr[i], end=" ")
+
+    buildHeap(arr, n-1)
+
+    print("\nAfter Heapify")
+    for i in range(1, n):
+        print(arr[i], end=" ")
+
+if __name__ == "__main__":
+    main()
