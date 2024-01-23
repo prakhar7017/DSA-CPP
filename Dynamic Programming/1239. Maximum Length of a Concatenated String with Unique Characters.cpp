72d6d30c65beda8f4f1396d541d8ff76ba3d9fcd
Prakhar Maheshwari <prakhar2115430@akgec.ac.in>
2024-01-23 19:04:28 +0530

    maximum length of a concatenated string with unique charachets python

--- Dynamic Programming/1239. Maximum Length of a Concatenated String with Unique Characters.cpp
+++ Dynamic Programming/1239. Maximum Length of a Concatenated String with Unique Characters.cpp
@@ -45,3 +45,43 @@ public:
     }
 };
 
+
+//**************************Python Solution**********************************
+from typing import List
+
+class Solution:
+    @staticmethod
+    def isDuplicate(s1, s2) -> bool:
+        arr = [0] * 26
+        for ch in s1:
+            if arr[ord(ch) - ord('a')] > 0:
+                return True
+            arr[ord(ch) - ord('a')] += 1
+        
+        for ch in s2:
+            if arr[ord(ch) - ord('a')] > 0:
+                return True
+        
+        return False
+
+    @staticmethod
+    def solve(arr, temp, i, n):
+        # base case
+        if i >= n:
+            return len(temp)
+
+        include = 0
+        exclude = 0
+        if Solution.isDuplicate(arr[i], temp):
+            exclude = Solution.solve(arr, temp, i + 1, n)
+        else:
+            include = Solution.solve(arr, temp + arr[i], i + 1, n)
+            exclude = Solution.solve(arr, temp, i + 1, n)
+
+        return max(include, exclude)
+
+    def maxLength(self, arr: List[str]) -> int:
+        temp = ""
+        n = len(arr)
+        index = 0
+        return Solution.solve(arr, temp, index, n)
