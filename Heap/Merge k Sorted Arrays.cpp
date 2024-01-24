66fdd387f2b562c51066b4534bcd14a61e04a46c
Prakhar Maheshwari <prakhar2115430@akgec.ac.in>
2024-01-24 20:15:05 +0530

    merge k sorted arrays

--- Heap/Merge k Sorted Arrays.cpp
+++ Heap/Merge k Sorted Arrays.cpp
@@ -18,3 +18,50 @@ class Solution
     }
 };
 
+//optimized solution using min-heap
+class Node {
+public:
+    int data;
+    int row;
+    int col;
+    Node(int data, int row, int col) : data(data), row(row), col(col) {};
+};
+
+class compare {
+public:
+    bool operator()(const Node* a, const Node* b) const {
+        return a->data > b->data;
+    }
+};
+
+class Solution {
+public:
+    vector<int> mergeKArrays(vector<vector<int>>& arr, int K) {
+        priority_queue<Node*, vector<Node*>, compare> pq;
+
+        // step 1: Insert the first element of each array into the min heap.
+        for (int i = 0; i < K; i++) {
+            Node* temp = new Node(arr[i][0], i, 0);
+            pq.push(temp);
+        }
+
+        vector<int> ans;
+
+        // step 2: Merge the arrays using the min heap.
+        while (!pq.empty()) {
+            Node* temp = pq.top();
+            pq.pop();
+            ans.push_back(temp->data);
+            int row = temp->row;
+            int col = temp->col;
+
+            if (col + 1 < arr[row].size()) {
+                Node* temp = new Node(arr[row][col + 1], row, col + 1);
+                pq.push(temp);
+            }
+        }
+
+        return ans;
+    }
+};
+
