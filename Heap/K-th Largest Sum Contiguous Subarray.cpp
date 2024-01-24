7887023dd6c299a0629173a0522c057740523f0a
Prakhar Maheshwari <prakhar2115430@akgec.ac.in>
2024-01-24 18:45:58 +0530

    k-th largest sum contignous subarray optimisez using min-heap

--- Heap/K-th Largest Sum Contiguous Subarray.cpp
+++ Heap/K-th Largest Sum Contiguous Subarray.cpp
@@ -17,3 +17,28 @@ public:
 };
 // Time complexity :o(n^2logn)
 // space complexity:o(n^2)
+
+// optimized solution using min-heap
+class Solution{
+public:
+    int kthLargest(vector<int> &Arr,int N,int K){
+        priority_queue<int,vector<int>,greater<int>>pq;
+        for(int i=0;i<N;i++){
+            int sum=0;
+            for(int j=i;j<N;j++){
+                sum+=Arr[j];
+                if(pq.size()<K){
+                    pq.push(sum);
+                }else{
+                    if(sum>pq.top()){
+                        pq.pop();
+                        pq.push(sum);
+                    }
+                }
+            }
+        }
+        return pq.top();
+    }
+};
+// time complexity:o(n^2);
+// space complexity:o(K);
