82d48c485babe4bb606c0861ab56e529276ed34f
Prakhar Maheshwari <prakhar2115430@akgec.ac.in>
2024-02-05 21:29:30 +0530

    first unique character in a string py

--- Strings/387. First Unique Character in a String.cpp
+++ Strings/387. First Unique Character in a String.cpp
@@ -17,3 +17,22 @@ public:
         return -1;
     }
 };
+
+//******************************Python Solution********************************
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        freq=[0]*26
+        n=len(s)
+        for i in range(n):
+            ch=s[i]
+            freq[ord(ch)-ord('a')]+=1
+        
+        for i in range(n):
+            ch=s[i]
+            if(freq[ord(ch)-ord('a')]==1):
+                return i
+
+        return -1
+        
+// Time Complexity: O(n)
+// Space Complexity: O(1)
