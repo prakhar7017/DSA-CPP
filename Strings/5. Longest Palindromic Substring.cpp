a5b2a773ab870b333d23e9089e77e9d25c906ee2
Prakhar Maheshwari <prakhar2115430@akgec.ac.in>
2024-02-20 00:24:09 +0530

    longest palindromic substring dp approach

--- Strings/5. Longest Palindromic Substring.cpp
+++ Strings/5. Longest Palindromic Substring.cpp
@@ -27,3 +27,31 @@ public:
 };
 
 // time complexity: O(n^3)
+
+
+// Recursive approach
+//class Solution {
+public:
+    int maxLen=1;
+    int start=0;
+    bool solve(string& s,int i,int j,vector<vector<int>>&dp){
+        // base case
+        if(i>=j){
+            return true;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        bool flag=false;
+        if(s[i]==s[j]){
+            flag=solve(s,i+1,j-1,dp);
+        }
+        if(flag){
+            int currLen=j-i+1;
+            if(currLen>maxLen){
+                maxLen=currLen;
+                start=i;
+            }
+        }
+        return dp[i][j]=flag;
+    }
