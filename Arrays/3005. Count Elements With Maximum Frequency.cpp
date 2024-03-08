059f7eb56bed65364e69b18d4996c3dbfea6ff48
Prakhar Maheshwari <prakhar2115430@akgec.ac.in>
2024-03-08 19:30:50 +0530

    Count Elements With Maximum Frequency

--- Arrays/3005. Count Elements With Maximum Frequency.cpp
+++ Arrays/3005. Count Elements With Maximum Frequency.cpp
@@ -19,3 +19,38 @@ public:
 
 // time complexity: O(n)
 // space complexity: O(n)
+
+class Solution {
+public:
+    int maxFrequencyElements(vector<int>& nums) {
+        int arr[101]={0};
+        int freq=0;
+        for(int i=0;i<nums.size();i++){
+            arr[nums[i]]++;
+            freq=max(freq,arr[nums[i]]);
+        }
+        return count(arr,arr+101,freq)*freq;
+    }
+};
+
+
+// one pass
+class Solution {
+public:
+    int maxFrequencyElements(vector<int>& nums) {
+        int maxFreq=0;
+        int ans=0;
+        vector<int>v(101);
+        for(int i=0;i<nums.size();i++){
+            v[nums[i]]++;
+            int freq= v[nums[i]];
+            if(freq>maxFreq){
+                maxFreq=freq;
+                ans=freq;
+            }else if(freq==maxFreq){
+                ans+=maxFreq;
+            }
+        }
+        return ans;
+    }
+};
