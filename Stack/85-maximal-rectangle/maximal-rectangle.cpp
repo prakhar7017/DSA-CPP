e1bcb9f0ab581fa34046382513650a091a719f5b
Prakhar Maheshwari <prakhar2115430@akgec.ac.in>
2024-04-14 03:02:22 +0530

    85. Maximal Rectangle

--- Stack/85-maximal-rectangle/maximal-rectangle.cpp
+++ Stack/85-maximal-rectangle/maximal-rectangle.cpp
@@ -1,45 +1,45 @@
 class Solution {
 public:
-    vector<int> getNSR(vector<int>&heights){
+    vector<int> getNSR(vector<int>&heights){ // next smaller element to the right
         int n=heights.size();
         stack<int>st;
         vector<int>NSR(n);
-        for(int i=n-1;i>=0;i--){
+        for(int i=n-1;i>=0;i--){ // right to left
             if(st.empty()){
-                NSR[i]=n;
+                NSR[i]=n; // if stack is empty, then there is no smaller element to the right
             }else{
-                while(!st.empty() && heights[st.top()]>=heights[i]){
+                while(!st.empty() && heights[st.top()]>=heights[i]){ // pop until we find a smaller element
                     st.pop();
                 }
-                if(st.empty()){
+                if(st.empty()){ // if stack is empty, then there is no smaller element to the right
                     NSR[i]=n;
-                }else{
+                }else{ // if stack is not empty, then the top element is the next smaller element to the right
                     NSR[i]=st.top();
                 }
               
             }
-            st.push(i);
+            st.push(i); // push the current element
         }
         return NSR;
     }
-    vector<int> getNSL(vector<int>&heights){
+    vector<int> getNSL(vector<int>&heights){ // next smaller element to the left
         int n=heights.size();
         stack<int>st;
         vector<int>NSL(n);
-        for(int i=0;i<n;i++){
-            if(st.empty()){
+        for(int i=0;i<n;i++){ // left to right
+            if(st.empty()){ // if stack is empty, then there is no smaller element to the left
                 NSL[i]=-1;
             }else{
-                while(!st.empty() && heights[st.top()]>=heights[i]){
+                while(!st.empty() && heights[st.top()]>=heights[i]){ // pop until we find a smaller element
                     st.pop();
                 }
-                if(st.empty()){
+                if(st.empty()){ // if stack is empty, then there is no smaller element to the left
                     NSL[i]=-1;
                 }else{
-                    NSL[i]=st.top();
+                    NSL[i]=st.top(); // if stack is not empty, then the top element is the next smaller element to the left
                 }
             }
-            st.push(i);
+            st.push(i); // push the current element
         }
         return NSL;
     }
@@ -51,15 +51,15 @@ public:
 
         int n=heights.size();
         vector<int>width(n);
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++){ // calculate the width of the rectangle
             width[i]=NSR[i]-NSL[i]-1;
         }
         int maxArea=0;
         for(int i=0;i<n;i++){
-            int area=width[i]*heights[i];
+            int area=width[i]*heights[i]; // calculate the area of the rectangle
             maxArea=max(maxArea,area);
         }
-        return maxArea;
+        return maxArea; // return the maximum area
     }
     int maximalRectangle(vector<vector<char>>& matrix) {
         int rows=matrix.size();
