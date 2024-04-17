e1fd78b5b31d3c2fbed165287e3d8ecb82d1098d
Prakhar Maheshwari <113543305+prakhar7017@users.noreply.github.com>
2024-04-17 21:57:28 +0530

    Time: 10 ms (48.64%) | Memory: 20.1 MB (82.98%) - LeetSync

--- 1030-smallest-string-starting-from-leaf/smallest-string-starting-from-leaf.cpp
+++ 1030-smallest-string-starting-from-leaf/smallest-string-starting-from-leaf.cpp
@@ -1,26 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     string ans;
-    void dfs(TreeNode* root,string curr){
-        // base case
-        if(!root) return ;
-
-        curr=char(root->val+'a')+curr;
-
-        //leaf node check
-        if(!root->left && !root->right) {
-            if(ans=="" || ans>curr) {
-                ans=curr;
-            }
-            return;
-        }
-
-        dfs(root->left,curr);
-        dfs(root->right,curr);
-    }
     string smallestFromLeaf(TreeNode* root) {
-        string curr="";
-        dfs(root,curr);
+        queue<pair<TreeNode*,string>> q;
+        // initial State
+        q.push({root,string(1,char(root->val+'a'))});
+
+        while(!q.empty()){
+            auto[node,curr]=q.front();
+            q.pop();
+
+            if(!node->left && !node->right) {
+                if(ans=="" || ans>curr){
+                    ans=curr;
+                }
+            }
+
+            if(node->left){
+                q.push({node->left,char(node->left->val+'a')+curr});
+            }
+            if(node->right){
+                q.push({node->right,char(node->right->val+'a')+curr});
+            }
+        }
         return ans;
     }
 };
