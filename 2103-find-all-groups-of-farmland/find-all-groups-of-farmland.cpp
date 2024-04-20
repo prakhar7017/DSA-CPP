916dc70bd5952aa2501e654670e7c4cefc6ead7e
Prakhar Maheshwari <113543305+prakhar7017@users.noreply.github.com>
2024-04-20 19:53:52 +0530

    Time: 142 ms (42.18%) | Memory: 137.2 MB (21.09%) - LeetSync

--- 2103-find-all-groups-of-farmland/find-all-groups-of-farmland.cpp
+++ 2103-find-all-groups-of-farmland/find-all-groups-of-farmland.cpp
@@ -22,6 +22,37 @@ public:
         }
         return;
     }
+
+    void bfs(vector<vector<int>>& land,int r1,int c1,int &r2,int &c2){
+        queue<pair<int,int>>q; // stores cordinate
+        // initial state
+        q.push({r1,c1});
+
+        while(!q.empty()){
+            pair<int,int>p=q.front(); // taking out the pair of cordinates
+            q.pop(); 
+            int currX=p.first;
+            int currY=p.second;
+            // visting mark
+            land[currX][currY]=0;
+            r2=max(currX,r2);
+            c2=max(currY,c2);
+
+            int dx[]={-1,0,1,0};
+            int dy[]={0,1,0,-1};
+
+            for(int i=0;i<4;i++){
+                int newR=currX+dx[i];
+                int newC=currY+dy[i];
+
+                if(newR>=0 && newR<rows && newC>=0 && newC<cols && land[newR][newC]==1){
+                    land[newR][newC]=0;
+                    q.push({newR,newC});
+                }
+            }
+        }
+    }
+
     vector<vector<int>> findFarmland(vector<vector<int>>& land) {
         rows=land.size();
         cols=land[0].size();
@@ -35,7 +66,7 @@ public:
                     int r2=row; // potential last row
                     int c2=col; // potential last col
 
-                    dfs(land,r1,c1,r2,c2);
+                    bfs(land,r1,c1,r2,c2);
                     ans.push_back({r1,c1,r2,c2});
                 }
             }
