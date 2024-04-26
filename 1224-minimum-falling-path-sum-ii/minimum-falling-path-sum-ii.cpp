9d8d71aeb851125f66d2438b2aca4d66d27f0600
Prakhar Maheshwari <113543305+prakhar7017@users.noreply.github.com>
2024-04-27 02:07:07 +0530

    Time: 28 ms (84.63%) | Memory: 18.2 MB (57.39%) - LeetSync

--- 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.cpp
+++ 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.cpp
@@ -47,6 +47,48 @@ public:
         }
         return result;
     }
+
+    int solveUsingTabOptimised(vector<vector<int>>& grid){
+        vector<vector<int>>dp(rows+1,vector<int>(cols+1,INT_MAX));
+
+        int firstSmallestCol=-1;
+        int secondSmallestCol=-1;
+        // setting up initial state, filling last row and calculating firstSmallestCol ,secondSmallesCol
+        for(int col=0;col<cols;col++){
+            dp[rows-1][col]=grid[rows-1][col];
+
+            if(firstSmallestCol==-1 ||  dp[rows-1][col]<=dp[rows-1][firstSmallestCol]){ // updation of firstSmallestCol
+                secondSmallestCol=firstSmallestCol;
+                firstSmallestCol=col;
+            } else if(secondSmallestCol==-1 || dp[rows-1][col]<=dp[rows-1][secondSmallestCol]){ // updation of secondSmallesCol
+                secondSmallestCol=col;
+            }
+        }
+
+        // moving in reverse dir
+        // from n-2 row to 0 row.
+        for(int row=rows-2;row>=0;row--){ 
+            int minCol1=-1;
+            int minCol2=-1;
+            for(int col=0;col<cols;col++){ // current col
+                if(col!=firstSmallestCol){
+                    dp[row][col]=grid[row][col]+dp[row+1][firstSmallestCol];
+                }else{
+                    dp[row][col]=grid[row][col]+dp[row+1][secondSmallestCol];
+                }
+
+                if(minCol1==-1 || dp[row][col]<=dp[row][minCol1]){
+                    minCol2=minCol1;
+                    minCol1=col;
+                }else if(minCol2==-1 || dp[row][col]<=dp[row][minCol2]){
+                    minCol2=col;
+                }
+            }
+            firstSmallestCol=minCol1;
+            secondSmallestCol=minCol2;
+        }
+        return dp[0][firstSmallestCol];
+    }
     int minFallingPathSum(vector<vector<int>>& grid) {
         rows=grid.size();
         cols=grid[0].size();
@@ -55,7 +97,7 @@ public:
         // for(int col=0;col<cols;col++){
         //     result=min(result,solveUsingMemo(0,col,grid,dp));
         // }
-        result=solveUsingTab(grid);
+        result=solveUsingTabOptimised(grid);
         return result;
     }
 };
