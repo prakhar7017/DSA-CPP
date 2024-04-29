681d5c0d33182dccc9e5b95cf2cba19e29ce2ce0
Prakhar Maheshwari <prakhar2115430@akgec.ac.in>
2024-04-30 00:35:27 +0530

    2997. Minimum Number of Operations to Make Array XOR Equal to K

--- 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
+++ 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int k) {
-        int totalXOR=0;
+        int totalXOR=0; // XOR of all elements in nums
 
         for(int num:nums){
-            totalXOR^=num;
+            totalXOR^=num; // XOR of all elements in nums
         }
 
-        int bitsDontMatch=totalXOR^k;
+        int bitsDontMatch=totalXOR^k; // XOR of all elements in nums and k which gives the bits dont match at every position
+        // for eg k=1-> 0 0 1
+        // totalXOR=4-> 1 0 0
+// bitsDontMatch=1^4->  1 0 1  , first and last bits dont match that why there is 1 at first and last position
 
-        int countBitsDontMatch=__builtin_popcount(bitsDontMatch);
+        int countBitsDontMatch=__builtin_popcount(bitsDontMatch); // count the number of bits dont match
 
         return countBitsDontMatch;
     }
 };
+
+// time complexity: O(n)
+// space complexity: O(1)
