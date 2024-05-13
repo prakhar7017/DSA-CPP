5b1b4b4722a09362ebbb969f7b0825f26d8c1ddf
Prakhar Maheshwari <113543305+prakhar7017@users.noreply.github.com>
2024-05-14 01:06:07 +0530

    Time: 2 ms (54.26%) | Memory: 10.1 MB (94.41%) - LeetSync

--- 891-score-after-flipping-matrix/score-after-flipping-matrix.cpp
+++ 891-score-after-flipping-matrix/score-after-flipping-matrix.cpp
@@ -4,39 +4,24 @@ public:
         int rows=grid.size();
         int cols=grid[0].size();
 
-        for(int row=0;row<rows;row++){
-            if(grid[row][0]==0){
-                for(int col=0;col<cols;col++){
-                    grid[row][col]=1-grid[row][col];
-                }
-            }
-        }
+        int sum=0;
+        sum+=rows*pow(2,cols-1);
 
         for(int col=1;col<cols;col++){
-            int countZeros=0;
+            // countSame bits compare with msb
+            int countSameBit=0;
             for(int row=0;row<rows;row++){
-                if(grid[row][col]==0){
-                    ++countZeros;
+                if(grid[row][col]==grid[row][0]){
+                    ++countSameBit;
                 }
             }
+            int countOnes=countSameBit;
+            int countZeros=rows-countOnes;
 
-            int countOnes=rows-countZeros;
+            int ones=max(countZeros,countOnes);
 
-            if(countZeros>countOnes){
-                for(int row=0;row<rows;row++){
-                    grid[row][col]=1-grid[row][col];
-                }
-            }
+            sum+=pow(2,cols-col-1)*ones;
         }
-
-        int sum=0;
-        for(int row=0;row<rows;row++){
-            for(int col=0;col<cols;col++){
-                sum+=grid[row][col]*pow(2,cols-col-1);
-            }
-        }
-
         return sum;
-
     }
 };
