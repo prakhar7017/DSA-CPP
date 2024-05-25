3be3bd37e0eb2592fbca1a6e79e0f52cde2148c3
Prakhar Maheshwari <113543305+prakhar7017@users.noreply.github.com>
2024-05-26 00:38:12 +0530

    Time: 4 ms (32.03%) | Memory: 9.1 MB (49.07%) - LeetSync

--- 140-word-break-ii/word-break-ii.cpp
+++ 140-word-break-ii/word-break-ii.cpp
@@ -1,36 +1,32 @@
 class Solution {
 public:
-    vector<string> solve(string &s, vector<string>& wordDict,int start,unordered_map<string,bool>&dict){
-        if(start>=s.size()){
-            return {""};
-        }
+    vector<string> solve(string &s,unordered_map<string,bool>&dict,int index){
+        // base case
+        if(index==s.length()) return {""};
+
         vector<string>ans;
-        string word;
+        string word="";
 
-        for(int i=start;i<s.length();i++){
+        for(int i=index;i<s.length();++i){
             word.push_back(s[i]);
-            if(dict.find(word)==dict.end()) { // not a valid part 
-                continue;
-            }
+            if(dict.find(word)==dict.end()) continue; // not found
 
-            // found a valid word
-            auto right=solve(s,wordDict,i+1,dict);
-            for(auto eachRight:right){
+            vector<string>right=solve(s,dict,i+1);
+            for(string eachRight:right){
                 string endPart;
                 if(eachRight.length()>0){
-                    endPart=" "+ eachRight;
+                    endPart=" "+eachRight;
                 }
                 ans.push_back(word+endPart);
             }
         }
         return ans;
-
     }
     vector<string> wordBreak(string s, vector<string>& wordDict) {
         unordered_map<string,bool>dict;
         for(auto word:wordDict){
             dict[word]=1;
         }
-        return solve(s,wordDict,0,dict);
+        return solve(s,dict,0);
     }
 };
