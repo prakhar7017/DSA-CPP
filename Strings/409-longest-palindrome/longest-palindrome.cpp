c83880f8595afd09ad0b696c19b110060dbe639d
Prakhar Maheshwari <prakhar2115430@akgec.ac.in>
2024-06-05 00:46:28 +0530

    409. Longest Palindrome

--- Strings/409-longest-palindrome/longest-palindrome.cpp
+++ Strings/409-longest-palindrome/longest-palindrome.cpp
@@ -23,3 +23,28 @@ public:
 
     }
 };
+
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        int n=s.length();
+        unordered_map<char,int>mp;
+        int oddFreq=0;
+
+        for(int i=0;i<n;i++){
+            char ch=s[i];
+            mp[ch]++;
+
+            if(mp[ch]&1){
+                ++oddFreq;
+            }else{
+                --oddFreq;
+            }
+        }
+        if(oddFreq>0){
+            return n-oddFreq+1;
+        }
+        return n;
+    }
+
+};
