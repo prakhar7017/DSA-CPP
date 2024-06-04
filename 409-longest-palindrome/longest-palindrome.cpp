4eb8209a568306b273d0282e5647af05bc03a7ff
Prakhar Maheshwari <113543305+prakhar7017@users.noreply.github.com>
2024-06-05 00:41:18 +0530

    Time: 5 ms (29.78%) | Memory: 9 MB (26.04%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.cpp
+++ 409-longest-palindrome/longest-palindrome.cpp
@@ -3,22 +3,23 @@ public:
     int longestPalindrome(string s) {
         int n=s.length();
         unordered_map<char,int>mp;
-        int oddFreq=0;
-
         for(int i=0;i<n;i++){
             char ch=s[i];
             mp[ch]++;
-
-            if(mp[ch]&1){
-                ++oddFreq;
+        }
+        int ans=0;
+        bool oddFreq=false;
+        for(auto it:mp){
+            if((it.second & 1) == 0){
+                ans+=it.second;
             }else{
-                --oddFreq;
+                ans+=it.second-1;
+                oddFreq=true;
             }
         }
-        if(oddFreq>0){
-            return n-oddFreq+1;
-        }
-        return n;
-    }
 
+        if(oddFreq==true) return ans+1;
+        return ans;
+
+    }
 };
