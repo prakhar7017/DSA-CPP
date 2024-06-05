a17afedc9d2b13310d0d86cef28caf52362c2a36
Prakhar Maheshwari <113543305+prakhar7017@users.noreply.github.com>
2024-06-06 00:35:32 +0530

    Time: 4 ms (84.24%) | Memory: 11.4 MB (69.23%) - LeetSync

--- 1044-find-common-characters/find-common-characters.cpp
+++ 1044-find-common-characters/find-common-characters.cpp
@@ -1,36 +1,31 @@
 class Solution {
 public:
-    vector<string> commonChars(vector<string>& words) {
-        vector<int> last = count(words[0]);
-        for (int i = 1; i < words.size(); i++) {
-            last = intersection(last, count(words[i]));
+    void fillMinFreq(string &word,int minFreq[26]){
+        for(char &ch:word){
+            minFreq[ch-'a']++;
         }
-        
-        vector<string> result;
-        for (int i = 0; i < 26; i++) {
-            while (last[i] > 0) {
-                result.push_back(string(1, 'a' + i));
-                last[i]--;
+    }
+    vector<string> commonChars(vector<string>& words) {
+        int n=words.size();
+
+        int minFreq[26]={0};
+
+        fillMinFreq(words[0],minFreq);
+
+        for(string word:words){
+            int temp[26]={0};
+            fillMinFreq(word,temp);
+            for(int i=0;i<26;i++){
+                minFreq[i]=min(minFreq[i],temp[i]);
             }
         }
-        
-        return result;
-    }
-    
-private:
-    vector<int> count(const string& str) {
-        vector<int> frequency(26, 0);
-        for (char c : str) {
-            frequency[c - 'a']++;
+        vector<string>ans;
+        for(int i=0;i<26;i++){
+            int freq=minFreq[i];
+            while(freq--){
+                ans.push_back(string(1,i+'a'));
+            }
         }
-        return frequency;
-    }
-    
-    vector<int> intersection(const vector<int>& a, const vector<int>& b) {
-        vector<int> t(26, 0);
-        for (int i = 0; i < 26; i++) {
-            t[i] = min(a[i], b[i]);
-        }
-        return t;
+        return ans;
     }
 };
