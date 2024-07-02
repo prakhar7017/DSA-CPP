322fd3ba0bac88f2d64ca76841cefd7852e73b41
Prakhar Maheshwari <prakhar2115430@akgec.ac.in>
2024-07-02 11:04:41 +0530

    547-number-of-provinces

--- Striver SDE Sheet/Graph/BFS/DFS Problems/547-number-of-provinces/number-of-provinces.cpp
+++ Striver SDE Sheet/Graph/BFS/DFS Problems/547-number-of-provinces/number-of-provinces.cpp
@@ -9,6 +9,22 @@ public:
             }
         }
     }
+    void bfs(int city,vector<bool>&vis,unordered_map<int,list<int>>&adjList){
+        queue<int>q;
+        q.push(city);
+        vis[city]=true;
+
+        while(!q.empty()){
+            int frontCity=q.front();
+            q.pop();
+            for(int &nbr:adjList[frontCity]){
+                if(!vis[nbr]){
+                    q.push(nbr);
+                    vis[nbr]=true;
+                }
+            }
+        }
+    }
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n=isConnected.size();
         unordered_map<int,list<int>>adjList;
@@ -26,7 +42,8 @@ public:
         int provinces=0;
         for(int city=0;city<n;city++){
             if(!vis[city]){
-                dfs(city,vis,adjList);
+                // dfs(city,vis,adjList);
+                bfs(city,vis,adjList);
                 provinces++;
             }
         }
