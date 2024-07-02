b76aefa48a280f1d8f787952e703f07c6ea897b3
Prakhar Maheshwari <113543305+prakhar7017@users.noreply.github.com>
2024-07-02 12:11:36 +0530

    Time: 20 ms (28.17%) | Memory: 19.9 MB (7.94%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -3,6 +3,8 @@ public:
     bool solveUsingKansAlgo(int &numCourses,vector<int>&indegree,unordered_map<int,list<int>>&adjList){
         queue<int>q;
         vector<int>topo;
+        vector<bool>vis(numCourses,false);
+        stack<int>st;
 
         for(int course=0;course<numCourses;course++){
             if(indegree[course]==0){
@@ -27,9 +29,27 @@ public:
         return topo.size()==numCourses;
 
     }
+
+    void solveUsingDFS(int course,stack<int>&st,vector<bool>&vis,vector<bool>&dfsCalls,unordered_map<int,list<int>>&adjList){
+        vis[course]=true;
+        dfsCalls[course]=true;
+        for(int &nbrCourse:adjList[course]){
+            if(dfsCalls[nbrCourse]==true){
+                return;
+            }
+            if(!vis[nbrCourse]){
+                solveUsingDFS(nbrCourse,st,vis,dfsCalls,adjList);
+            }
+        }
+        dfsCalls[course]=false;
+        st.push(course);
+    }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<int>indegree(numCourses,0);
         unordered_map<int,list<int>>adjList;
+        vector<bool>vis(numCourses,false);
+        vector<bool>dfsCalls(numCourses,false);
+        stack<int>st;
 
         for(vector<int>&vec:prerequisites){
             int u=vec[1];
@@ -38,8 +58,22 @@ public:
             adjList[u].push_back(v);
         }
 
-        bool ans=solveUsingKansAlgo(numCourses,indegree,adjList);
+        bool ans=false;
+        // bool ans=solveUsingKansAlgo(numCourses,indegree,adjList);
 
-        return ans;
+        for(int course=0;course<numCourses;course++){
+            if(!vis[course]){
+                solveUsingDFS(course,st,vis,dfsCalls,adjList);
+            }
+        }
+        vector<int>result;
+        while(!st.empty()){
+            result.push_back(st.top());
+            st.pop();
+        }
+        if(result.size()==numCourses){
+            return true;
+        }
+        return false;
     }
 };
