f0f9f9734c91b926f9e04f7e9518e1f3002e7bd1
Prakhar Maheshwari <113543305+prakhar7017@users.noreply.github.com>
2024-07-04 22:14:58 +0530

    Time: 468 ms (26.98%) | Memory: 266.8 MB (62.14%) - LeetSync

---
+++ 2299-merge-nodes-in-between-zeros/merge-nodes-in-between-zeros.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    ListNode* mergeNodes(ListNode* head) {
+        if(!head){
+            return NULL;
+        }
+        ListNode* slow=head;
+        ListNode* fast=head->next;
+        ListNode* newLastNode=NULL;
+        int sum=0;
+        while(fast){
+            if(fast->val!=0){
+                sum+=fast->val;
+            }else{
+                // means 2 consu. 0 nodes ke bech ka sum prepare hogya hoga
+                slow->val=sum;
+                newLastNode=slow;
+                slow=slow->next;
+                sum=0;
+            }
+            fast=fast->next;
+        }
+
+        ListNode* temp=slow; // storing unwanted list for deletion
+        newLastNode->next=NULL; // desiredlist->next=NULL
+
+        while(temp){ // deleting unwanted list for preventing memory leak 
+            ListNode* nxt=temp->next;
+            delete temp;
+            temp=nxt;
+        }
+        return head;
+    }
+};
