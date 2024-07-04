85583c459e5d077cdf80e989081d32fdc07b1d1b
Prakhar Maheshwari <113543305+prakhar7017@users.noreply.github.com>
2024-07-04 13:53:48 +0530

    Time: 82 ms (21.35%) | Memory: 35.5 MB (17.50%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -4,6 +4,7 @@ public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int rows=grid.size();
         int cols=grid[0].size();
+        vector<vector<int>>dist(rows+1,vector<int>(cols+1,INT_MAX));
 
         if(rows==0 || cols==0 || grid[0][0]==1){
             return -1;
@@ -13,36 +14,43 @@ public:
             return newX>=0 && newX<rows && newY>=0 && newY<cols;
         };
 
-        queue<pair<int,int>>q;
-        int level=0;
+        set<pair<int,pair<int,int>>>st;
 
-        q.push({0,0});
+        st.insert({0,{0,0}});
         grid[0][0]=1;
+        dist[0][0]=0;
 
-        while(!q.empty()){
-            int n=q.size();
-            while(n--){
-                pair<int,int> frontPair=q.front();
-                q.pop();
-                int x=frontPair.first;
-                int y=frontPair.second;
+        while(!st.empty()){
+            auto topElement=st.begin();
+            auto topPair=*topElement;
+            int srcDist=topPair.first;
+            pair<int,int>srcCorr=topPair.second;
+            int x=srcCorr.first;
+            int y=srcCorr.second;
+            st.erase(st.begin());
 
-                if(x==rows-1 && y==cols-1){
-                    return level+1;
-                }
+            for(vector<int>&dir:directions){
+                int new_x=x+dir[0];
+                int new_y=y+dir[1];
 
-                for(vector<int> dir:directions){
-                    int new_x=x+dir[0];
-                    int new_y=y+dir[1];
+                int nbrDist=1;
+                if(isSafe(new_x,new_y) && grid[new_x][new_y]==0){
 
-                    if(isSafe(new_x,new_y) && grid[new_x][new_y]==0){
-                        q.push({new_x,new_y});
+                    if(srcDist+nbrDist<dist[new_x][new_y]){
+                        auto prevEntry=st.find({dist[new_x][new_y],{new_x,new_y}});
+                        if(prevEntry!=st.end()){
+                            st.erase(prevEntry);
+                        }
+                        dist[new_x][new_y]=srcDist+nbrDist;
                         grid[new_x][new_y]=1;
+                        st.insert({dist[new_x][new_y],{new_x,new_y}});
                     }
                 }
             }
-            level++;
         }
-        return -1;
+        if(dist[rows-1][cols-1]==INT_MAX){
+            return -1;
+        }
+        return dist[rows-1][cols-1]+1;
     }
 };
