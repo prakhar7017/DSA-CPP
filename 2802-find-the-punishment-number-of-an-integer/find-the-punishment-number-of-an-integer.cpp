db62f249476c1261c474e634b124c501d0631a35
Prakhar Maheshwari <113543305+prakhar7017@users.noreply.github.com>
2025-02-16 00:51:57 +0530

    Time: 764 ms (9.07%) | Memory: 539 MB (5.54%) - LeetSync

--- 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.cpp
+++ 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.cpp
@@ -1,29 +1,32 @@
 class Solution {
 public:
-    bool check(int index,int currSum,string s,int &target){
+    bool check(int index,int currSum,string s,int &target,vector<vector<int>>&v){
         if(index==s.length()) {
             if(currSum==target) return true;
             else return false;
         }
-
         if(currSum > target) return false;
+
+        if(v[index][currSum]!=-1) return v[index][currSum];
+
 
         bool possible=false;
         for(int j=index;j<s.length();j++){
             string sub=s.substr(index,j-index+1);
             int val=stoi(sub);
-            possible=possible || check(j+1,currSum+val,s,target);
+            possible=possible || check(j+1,currSum+val,s,target,v);
 
             if(possible==true) return true;
         }
-        return false;
+        return v[index][currSum]=possible;
     }
     int punishmentNumber(int n) {
         int punish=0;
         for(int num=1;num<=n;num++){
             int sq=num*num;
             string s=to_string(sq);
-            if(check(0,0,s,num)){
+            vector<vector<int>>v(s.length(),vector<int>(num+1,-1));
+            if(check(0,0,s,num,v)){
                 punish+=sq;
             }
         }
