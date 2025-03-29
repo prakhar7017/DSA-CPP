28327d55beb69b7356876e5afb8797a192ddfb71
Prakhar Maheshwari <113543305+prakhar7017@users.noreply.github.com>
2025-03-30 02:03:27 +0530

    Time: 1 ms (93.48%) | Memory: 46.6 MB (7.57%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.java
+++ 724-find-pivot-index/find-pivot-index.java
@@ -1,16 +1,35 @@
 class Solution {
-    public int pivotIndex(int[] nums) {
+    public int solve(int[] nums){
         int n=nums.length;
+        int[] leftsum=new int[n];
+        int[] rightsum=new int[n];
 
+        leftsum[0]=0;
+        rightsum[n-1]=0;
+        for(int i=1;i<n;i++){
+            leftsum[i]=leftsum[i-1]+nums[i-1];
+        }
+        for(int i=n-2;i>=0;i--){
+            rightsum[i]=rightsum[i+1]+nums[i+1];
+        }
         for(int i=0;i<n;i++){
-            int leftsum=0;
-            int rightsum=0;
-            for(int j=0;j<i;j++){
-                leftsum+=nums[j];
-            }
-            for(int j=i+1;j<n;j++) rightsum+=nums[j];
-            if(leftsum==rightsum) return i;
+            if(leftsum[i]==rightsum[i]) return i;
         }
         return -1;
+    }
+    public int pivotIndex(int[] nums) {
+        // int n=nums.length;
+
+        // for(int i=0;i<n;i++){
+        //     int leftsum=0;
+        //     int rightsum=0;
+        //     for(int j=0;j<i;j++){
+        //         leftsum+=nums[j];
+        //     }
+        //     for(int j=i+1;j<n;j++) rightsum+=nums[j];
+        //     if(leftsum==rightsum) return i;
+        // }
+        // return -1;
+        return solve(nums);
     }
 }
