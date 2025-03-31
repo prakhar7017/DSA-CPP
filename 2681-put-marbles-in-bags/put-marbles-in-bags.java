d77119439afe297e67cffe5f8387ca0cc07a1213
Prakhar Maheshwari <113543305+prakhar7017@users.noreply.github.com>
2025-04-01 00:42:36 +0530

    Time: 100 ms (20.90%) | Memory: 55.6 MB (95.52%) - LeetSync

---
+++ 2681-put-marbles-in-bags/put-marbles-in-bags.java
@@ -0,0 +1,21 @@
+class Solution {
+    public long putMarbles(int[] weights, int k) {
+        int n=weights.length;
+        List<Integer> pairSum=new ArrayList<>();
+        for(int i=0;i<n-1;i++){
+            pairSum.add(weights[i]+weights[i+1]);
+        }
+
+        Collections.sort(pairSum);
+
+        long maxSum=0;
+        long minSum=0;
+
+        for(int i=0;i<k-1;i++){
+            minSum+=pairSum.get(i);
+            maxSum+=pairSum.get(n-i-2);
+        }
+
+        return maxSum-minSum;
+    }
+}
