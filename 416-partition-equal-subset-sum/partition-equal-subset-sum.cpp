8a90361b785df25384d0ad5bcbe8e2f9f734e698
Prakhar Maheshwari <113543305+prakhar7017@users.noreply.github.com>
2025-04-07 12:08:38 +0530

    Time: 179 ms (17.17%) | Memory: 101.2 MB (17.78%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,73 +1,23 @@
 class Solution {
 public:
-    bool solveUsingRec(vector<int>& nums,int targetSum,int index,vector<vector<int>>&dp){
-        if(index>=nums.size() || targetSum<0){
-            return false;
-        }
-        if(targetSum==0){
-            return true;
-        }
-        if(dp[index][targetSum]!=-1){
-            return dp[index][targetSum];
-        }
-
-        bool include=solveUsingRec(nums,targetSum-nums[index],index+1,dp);
-        bool exclude=solveUsingRec(nums,targetSum,index+1,dp);
-
-        return dp[index][targetSum]=include || exclude ;
-    }
-    bool solveUsingTab(vector<int>& nums,int targetSum){
-        int n=nums.size();
-        vector<vector<int>>dp(n+2,vector<int>(targetSum+2,0));
-
-        for(int row=0;row<n;row++){
-            dp[row][targetSum]=0;
-        }
-
-
-        for(int index=n-1;index>=0;index--){
-            for(int target=targetSum;target>=0;target--){
-                bool include=dp[target-nums[index]][index+1];
-                bool exclude=dp[target][index+1];
-
-                dp[target][index]=include || exclude ;
-            }
-        }
-
-        return dp[targetSum][0];
-    }
-    bool solve(int index,int targetSum,vector<int>& nums,vector<vector<int>>&dp){
-        if(index>=nums.size()) return false;
-        if(targetSum<0) return false;
+    bool solveUsingRec(vector<int>& nums,int index,int targetSum,vector<vector<int>>&dp)    {
+        if(index>=nums.size() || targetSum<0 ) return false;
+        // if(index==nums.size() && targetSum==0) return true;
         if(targetSum==0) return true;
-        if(dp[index][targetSum]!=-1){
-            return dp[index][targetSum];
-        }
-
-        int exclude=solve(index+1,targetSum,nums,dp);
-        int include=solve(index+1,targetSum-nums[index],nums,dp);
+        if(dp[index][targetSum]!=-1) return dp[index][targetSum];
+        bool include=solveUsingRec(nums,index+1,targetSum-nums[index],dp);
+        bool exclude=solveUsingRec(nums,index+1,targetSum,dp);
         return dp[index][targetSum]=include || exclude;
+
     }
     bool canPartition(vector<int>& nums) {
-        // int index=0;
-        // int totalSum=accumulate(nums.begin(),nums.end(),0);
+        int n=nums.size();
+        int sum=accumulate(begin(nums),end(nums),0);
+        if(sum & 1) return false;
 
-        // if(totalSum & 1){
-        //     // odd sum cannot be partioned
-        //     return false;
-        // }
-        // int targetSum=totalSum>>1;
-        // // vector<vector<int>>dp(nums.size()+1,vector<int>(targetSum+1,-1));
-        // return solveUsingTab(nums,targetSum);
-
-        int totalSum=accumulate(nums.begin(),nums.end(),0);
-        int index=0;
-        if(totalSum & 1){
-            return false;
-        }
-        int targetSum=totalSum>>1;
-        vector<vector<int>>dp(nums.size()+1,vector<int>(targetSum+1,-1));
-        return solve(index,targetSum,nums,dp);
+        int targetSum=sum>>1;
+        vector<vector<int>>dp(n+1,vector<int>(targetSum+1,-1));
+        return solveUsingRec(nums,0,targetSum,dp);
 
     }
 };
