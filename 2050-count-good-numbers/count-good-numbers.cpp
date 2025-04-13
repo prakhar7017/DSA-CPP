e13132c46f0bfa01e601033c8583d9281505b8ba
Prakhar Maheshwari <113543305+prakhar7017@users.noreply.github.com>
2025-04-13 14:30:37 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (34.93%) - LeetSync

--- 2050-count-good-numbers/count-good-numbers.cpp
+++ 2050-count-good-numbers/count-good-numbers.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
-    const int M = 1e9+7;
-    int findPower(long long a, long long b) {
-        if(b == 0) {
-            return 1;
-        }   
+    const int Mod=1e9+7;
+    int findPower(long long a,long long b){
+        if(b==0) return 1;
 
-        long long half = findPower(a, b/2);
-        long long result = (half * half) % M;
-
-        if(b % 2 == 1) {
-            result = (result * a) % M;
+        long long half=findPower(a,b/2);
+        long long result=(half*half) % Mod;
+        if(b%2==1){
+            result=(a*result)%Mod;
         }
-
         return result;
-    }
 
+    }
     int countGoodNumbers(long long n) {
-        return (long long)findPower(5, (n+1)/2) * findPower(4, n/2) % M;
+        long long noOfEvenIndices=(n+1)/2;
+        long long noOfOddIndices=n/2;
+        return (long long)findPower(5,noOfEvenIndices)*findPower(4,noOfOddIndices)%Mod;
+
     }
 };
