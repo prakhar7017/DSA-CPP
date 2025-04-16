7cd257b408b2164e3d5d00a236d2e29c07d5844b
Prakhar Maheshwari <113543305+prakhar7017@users.noreply.github.com>
2025-04-16 21:46:04 +0530

    Time: 75 ms (82.94%) | Memory: 79.2 MB (54.98%) - LeetSync

---
+++ 2626-count-the-number-of-good-subarrays/count-the-number-of-good-subarrays.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    long long countGood(vector<int>& nums, int k) {
+        int n=nums.size();
+        long long ans=0;
+        long long pairs=0;
+        unordered_map<int,int>mp;
+        int i=0;
+        int j=0;
+        while(j<n){
+            pairs+=mp[nums[j]];
+            mp[nums[j]]++;
+            while(pairs>=k){
+                ans+=(n-j);
+                mp[nums[i]]--;
+                pairs-=mp[nums[i]];
+                i++;
+            }
+            j++;
+        }
+        return ans;
+    }
+};
