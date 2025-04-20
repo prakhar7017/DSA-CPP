cd6cb14a44132297a35ca051a14107b626af1150
Prakhar Maheshwari <113543305+prakhar7017@users.noreply.github.com>
2025-04-20 13:30:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find Only Repetitive Element from 1 to n-1/find-only-repetitive-element-from-1-to-n1.java
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            // Read the array from input line
+            String inputLine = read.readLine().trim();
+            String[] inputArr = inputLine.split("\\s+");
+            int n = inputArr.length;
+            int arr[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputArr[i]);
+            }
+
+            Solution ob = new Solution();
+            System.out.println(ob.findDuplicate(arr));
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+class Solution {
+    public int findDuplicate(int[] arr) {
+        int n=arr.length;
+        for(int i=0;i<n;i++){
+            int num=Math.abs(arr[i]);
+            if((arr[num-1])<0){
+                return num;
+            }
+            arr[num-1]*=-1;
+        }
+        return -1;
+    }
+}
