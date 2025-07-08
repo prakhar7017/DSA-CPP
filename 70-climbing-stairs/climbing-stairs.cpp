581c1a82f69c65b6998703166cfc8a74cd564153
Prakhar Maheshwari <113543305+prakhar7017@users.noreply.github.com>
2025-07-09 00:00:16 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (18.26%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -11,9 +11,20 @@ public:
         if(dp[n]!=0) return dp[n];
         return dp[n]=solveUsingDP(n-1,dp)+solveUsingDP(n-2,dp);
     }
+    int solveUsingBottomUp(int n){
+        vector<int>dp(n+1,-1);
+
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
     int climbStairs(int n) {
         // return solveUsingRec(n);
         vector<int>dp(n+1,0);
-        return solveUsingDP(n,dp);
+        // return solveUsingDP(n,dp);
+        return solveUsingBottomUp(n);
     }
 };
