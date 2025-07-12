4642c4cb3d49cf4e3ebae8965f21538601bc419b
Prakhar Maheshwari <113543305+prakhar7017@users.noreply.github.com>
2025-07-13 02:19:02 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (97.41%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -24,6 +24,20 @@ public:
         }
         return dp[start];
     }
+    int solveUsingSpace(int start,int end,vector<int>&nums){
+        int prev=nums[end];
+        int next=0;
+        int curr;
+        for(int i=end-1;i>=start;i--){
+            int include=nums[i]+next;
+            int exclude=0+prev;
+            curr=max(include,exclude);
+            next=prev;
+            prev=curr;
+        }
+        return prev;
+
+    }
     int rob(vector<int>& nums) {
         int n = nums.size();
         if (n == 1)
@@ -32,10 +46,10 @@ public:
             return max(nums[0], nums[1]);
 
         // Case 1: Rob from house 0 to n-2
-        int take0thHouse = solveUsingTab(0,n-2,nums);
+        int take0thHouse = solveUsingSpace(0,n-2,nums);
 
         // Case 2: Rob from house 1 to n-1
-        int take1stHouse = solveUsingTab(1,n-1,nums);
+        int take1stHouse = solveUsingSpace(1,n-1,nums);
 
         return max(take0thHouse, take1stHouse);
     }
