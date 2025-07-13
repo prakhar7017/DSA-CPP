0b058d95134648c0bd991d3e51a8b89a29c35902
Prakhar Maheshwari <113543305+prakhar7017@users.noreply.github.com>
2025-07-14 01:43:08 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (33.71%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -17,8 +17,22 @@ public:
         }
         return grid[x][y]=totalPaths;
     }
+    int solveUsingTab(int rows,int cols){
+        vector<vector<int>>grid(rows,vector<int>(cols,0));
+        grid[0][0]=1;
+        for(int col=1;col<cols;col++) grid[0][col]=1;
+        for(int row=1;row<rows;row++) grid[row][0]=1;
+
+        for(int row=1;row<rows;row++){
+            for(int col=1;col<cols;col++){
+                grid[row][col]=grid[row-1][col]+grid[row][col-1];
+            }
+        }
+        return grid[rows-1][cols-1];
+    }
     int uniquePaths(int m, int n) {
-        vector<vector<int>>grid(m,vector<int>(n,-1));
-        return solve(0,0,m,n,grid);
+        // vector<vector<int>>grid(m,vector<int>(n,-1));
+        // return solve(0,0,m,n,grid);
+        return solveUsingTab(m,n);
     }
 };
