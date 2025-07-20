3776cf598c412a5720569b729bae0588ab826b13
Prakhar Maheshwari <113543305+prakhar7017@users.noreply.github.com>
2025-07-21 01:28:33 +0530

    Time: 151 ms (35.26%) | Memory: 17.2 MB (72.32%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,23 +1,41 @@
 class Solution {
 public:
-    bool solveUsingRec(vector<int>& nums,int index,int targetSum,vector<vector<int>>&dp)    {
-        if(index>=nums.size() || targetSum<0 ) return false;
-        // if(index==nums.size() && targetSum==0) return true;
-        if(targetSum==0) return true;
-        if(dp[index][targetSum]!=-1) return dp[index][targetSum];
-        bool include=solveUsingRec(nums,index+1,targetSum-nums[index],dp);
-        bool exclude=solveUsingRec(nums,index+1,targetSum,dp);
-        return dp[index][targetSum]=include || exclude;
+    bool solve(int index, int target, vector<int>& nums) {
+        // basecase
+        if (target == 0)
+            return 1;
+        if (index >= nums.size())
+            return 0;
 
+        int exclude = solve(index + 1, target, nums);
+        int include = 0;
+        if (nums[index] <= target)
+            include = solve(index + 1, target - nums[index], nums);
+        return include || exclude;
+    }
+
+    bool solveUsingTab(int target,vector<int>& nums){
+        int n=nums.size();
+        vector<vector<bool>>dp(n,vector<bool>(target+1,false));
+        for(int i=0;i<n;i++) dp[i][0]=true;
+        if(nums[0]<=target) dp[0][nums[0]]=true;
+        for(int i=1;i<n;i++){
+            for(int tar=1;tar<=target;tar++){
+                bool exclude=dp[i-1][tar];
+                bool include=false;
+                if(nums[i]<=tar) include=dp[i-1][tar-nums[i]];
+                dp[i][tar]=(include || exclude);
+            }
+        }
+        return dp[n-1][target];
     }
     bool canPartition(vector<int>& nums) {
-        int n=nums.size();
-        int sum=accumulate(begin(nums),end(nums),0);
-        if(sum & 1) return false;
-
-        int targetSum=sum>>1;
-        vector<vector<int>>dp(n+1,vector<int>(targetSum+1,-1));
-        return solveUsingRec(nums,0,targetSum,dp);
-
+        int n = nums.size();
+        int sum = accumulate(begin(nums), end(nums),0);
+        if (sum & 1)
+            return false;
+        int targetSum = sum >> 1;
+        // return solve(0, targetSum, nums);
+        return solveUsingTab(targetSum,nums);
     }
 };
