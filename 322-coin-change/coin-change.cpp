844cc7e88eb5242223f05d0ed1e929489ef010f5
Prakhar Maheshwari <113543305+prakhar7017@users.noreply.github.com>
2025-07-25 03:07:33 +0530

    Time: 46 ms (39.63%) | Memory: 49.5 MB (24.22%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -16,11 +16,34 @@ public:
         }
         return dp[amount] = minCoins;
     }
+
+    int solve1(int index, vector<int>& coins, int amount,
+               vector<vector<int>>& dp) {
+        if (index == 0) {
+            if (amount % coins[0] == 0)
+                return amount / coins[0];
+            return INT_MAX;
+        }
+
+        if (dp[index][amount] != -1)
+            return dp[index][amount];
+
+        int exclude = solve1(index - 1, coins, amount, dp);
+        int include = INT_MAX;
+        if (coins[index] <= amount) {
+            int subRes = solve1(index, coins, amount - coins[index], dp);
+            if (subRes != INT_MAX)
+                include = 1 + subRes;
+        }
+
+        return dp[index][amount] = min(include, exclude);
+    }
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<int> dp(amount+1,-1);
-        int ans = solve(n, coins, amount, dp);
-        if (ans == INT_MAX)
+        // vector<int> dp(amount + 1, -1);
+        vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
+        int ans = solve1(n-1, coins, amount, dp);
+        if (ans >= 1e9)
             return -1;
         else
             return ans;
