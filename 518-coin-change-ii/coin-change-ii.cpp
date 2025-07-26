cc333df79466b07fece5257c6d811912d5cb516a
Prakhar Maheshwari <113543305+prakhar7017@users.noreply.github.com>
2025-07-27 02:42:46 +0530

    Time: 27 ms (36.28%) | Memory: 46 MB (19.43%) - LeetSync

---
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -0,0 +1,39 @@
+const int MOD=1e9+7;
+class Solution {
+public:
+    int solve(int index, int amount, vector<int>& coins) {
+        // basecase
+        if(index==0){
+            if(amount%coins[index]==0) return 1;
+            return 0;
+        }
+        int exclude = solve(index - 1, amount, coins);
+        int include = 0;
+        if (coins[index] <= amount)
+            include = solve(index, amount - coins[index], coins);
+
+        return (include+exclude);    
+    }
+    int solveUsingTab(int amount, vector<int>& coins){
+        int n=coins.size();
+        vector<vector<unsigned long long>>dp(n,vector<unsigned long long>(amount+1,0));
+        for(int amt=0;amt<=amount;amt++){
+            if(amt%coins[0]==0) dp[0][amt]=1;
+        }
+
+        for(int i=1;i<n;i++){
+            for(int amt=0;amt<=amount;amt++){
+                unsigned long long exclude=dp[i-1][amt];
+                unsigned long long include=0;
+                if(coins[i]<=amt) include=dp[i][amt-coins[i]];
+                dp[i][amt]=(include+exclude);
+            }
+        }
+        return dp[n-1][amount];
+    }
+    int change(int amount, vector<int>& coins) {
+        int n = coins.size();
+        // return solve(n - 1, amount, coins);
+        return solveUsingTab(amount,coins);
+    }
+};
