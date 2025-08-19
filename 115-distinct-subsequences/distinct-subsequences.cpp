48ff08efaba58a4b7e53759f856081b6150d07ee
Prakhar Maheshwari <113543305+prakhar7017@users.noreply.github.com>
2025-08-20 02:09:35 +0530

    Time: 31 ms (54.78%) | Memory: 44.4 MB (14.32%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,19 +1,67 @@
 class Solution {
+    int mod=1e9+7;
 public:
-    int solveUsingRec(int n,int m,string &s, string &t,vector<vector<int>>&dp){
+    int solveUsingRec(int n, int m, string& s, string& t,
+                      vector<vector<int>>& dp) {
         // basecase
-        if(m==0) return 1;
-        if(n==0) return 0;
-        if(dp[n][m]!=-1) return dp[n][m];
-        int ans=0;
-        if(s[n-1]==t[m-1]) ans+=solveUsingRec(n-1,m-1,s,t,dp);
-        ans+=solveUsingRec(n-1,m,s,t,dp);
-        return dp[n][m]=ans;
+        if (m == 0)
+            return 1;
+        if (n == 0)
+            return 0;
+        if (dp[n][m] != -1)
+            return dp[n][m];
+        int ans = 0;
+        if (s[n - 1] == t[m - 1])
+            ans += solveUsingRec(n - 1, m - 1, s, t, dp);
+        ans += solveUsingRec(n - 1, m, s, t, dp);
+        return dp[n][m] = ans;
+    }
+    int solveUsingTab(string& s, string& t) {
+        int n = s.length();
+        int m = t.length();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 1;
+        }
+        for (int j = 1; j <= m; j++) {
+            dp[0][j] = 0;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                int ans = 0;
+                if (s[i - 1] == t[j - 1])
+                    ans += dp[i - 1][j - 1];
+                ans += dp[i - 1][j];
+                dp[i][j]=ans;
+            }
+        }
+        return dp[n][m];
+    }
+    int solveUsingSpace(string& s, string& t) {
+        int n = s.length();
+        int m = t.length();
+        vector<int>prev(m + 1, 1);
+        for (int i = 1; i <= m; i++) {
+            prev[i]= 0;
+        }
+        for (int i = 1; i <= n; i++) {
+            vector<int>curr(m+1,1);
+            for (int j = 1; j <= m; j++) {
+                int ans = 0;
+                if (s[i - 1] == t[j - 1])
+                    ans += prev[j - 1];
+                ans += prev[j];
+                curr[j]=ans%mod;
+            }
+            prev=curr;
+        }
+        return prev[m];
     }
     int numDistinct(string s, string t) {
-        int n=s.length();
-        int m=t.length();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        return solveUsingRec(n,m,s,t,dp);
+        int n = s.length();
+        int m = t.length();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
+        return solveUsingSpace(s, t);
     }
 };
