f5744fd94675a3ed81c0852140ee47f5c3569612
Prakhar Maheshwari <113543305+prakhar7017@users.noreply.github.com>
2025-08-24 01:51:23 +0530

    Time: 3 ms (42.21%) | Memory: 22.2 MB (14.94%) - LeetSync

---
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int solveUsingRec(int index, int buy, vector<int>& prices,
+                      vector<vector<int>>& dp) {
+        if (index >= prices.size())
+            return 0;
+        if (dp[index][buy] != -1)
+            return dp[index][buy];
+        int profit = 0;
+        if (buy) {
+            profit =
+                max(-prices[index] + solveUsingRec(index + 1, 0, prices, dp),
+                    0 + solveUsingRec(index + 1, 1, prices, dp));
+
+        } else {
+            profit =
+                max(prices[index] + solveUsingRec(index + 1, 1, prices, dp),
+                    0 + solveUsingRec(index + 1, 0, prices, dp));
+        }
+        return dp[index][buy] = profit;
+    }
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<int>> dp(n, vector<int>(2, -1));
+        return solveUsingRec(0, 1, prices, dp);
+    }
+};
