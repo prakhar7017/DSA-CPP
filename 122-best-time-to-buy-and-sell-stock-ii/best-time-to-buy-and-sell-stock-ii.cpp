36a7c9e64d0262f8f2657a22c7e0d8a6e87d4eec
Prakhar Maheshwari <113543305+prakhar7017@users.noreply.github.com>
2025-08-24 02:35:21 +0530

    Time: 0 ms (100.00%) | Memory: 17.1 MB (70.18%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,72 +1,19 @@
 class Solution {
 public:
-    int solveUsingRec(int index, int buy, vector<int>& prices,
-                      vector<vector<int>>& dp) {
-        if (index >= prices.size())
-            return 0;
-        if (dp[index][buy] != -1)
-            return dp[index][buy];
-        int profit = 0;
-        if (buy) {
-            profit =
-                max(-prices[index] + solveUsingRec(index + 1, 0, prices, dp),
-                    0 + solveUsingRec(index + 1, 1, prices, dp));
-
-        } else {
-            profit =
-                max(prices[index] + solveUsingRec(index + 1, 1, prices, dp),
-                    0 + solveUsingRec(index + 1, 0, prices, dp));
-        }
-        return dp[index][buy] = profit;
-    }
-
-    int solveUsingRec(vector<int>& prices) {
+    int solveSpaceOptimized1(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> dp(n + 1, vector<int>(2, 0));
-        // basecase
-        dp[n][0] = dp[n][1] = 0;
-        int profit;
+        int nextBuy, nextNotBuy, currBuy, currNotBuy;
+        nextBuy = nextNotBuy = 0;
+
         for (int index = n - 1; index >= 0; index--) {
-            for (int buy = 1; buy >= 0; buy--) {
-                if (buy) {
-                    profit = max(-prices[index] + dp[index + 1][0],
-                                 0 + dp[index + 1][1]);
-
-                } else {
-                    profit = max(prices[index] + dp[index + 1][1],
-                                 0 + dp[index + 1][0]);
-                }
-                dp[index][buy] = profit;
-            }
+            currBuy = max(-prices[index] + nextNotBuy, 0 + nextBuy);
+            currNotBuy = max(prices[index] + nextBuy, 0 + nextNotBuy);
+            nextBuy = currBuy;
+            nextNotBuy = currNotBuy;
         }
-        return dp[0][1];
-    }
-
-    int solveUsingSpaceOptimized(vector<int>& prices) {
-        int n = prices.size();
-        vector<int> next(2, 0);
-        // basecase
-        next[0] = next[1] = 0;
-        int profit;
-        for (int index = n - 1; index >= 0; index--) {
-            vector<int> curr(2, 0);
-            for (int buy = 1; buy >= 0; buy--) {
-                if (buy) {
-                    profit = max(-prices[index] + next[0], 0 + next[1]);
-
-                } else {
-                    profit = max(prices[index] + next[1], 0 + next[0]);
-                }
-                curr[buy] = profit;
-                next=curr;
-            }
-        }
-        return next[1];
+        return nextBuy;
     }
     int maxProfit(vector<int>& prices) {
-        int n = prices.size();
-        vector<vector<int>> dp(n, vector<int>(2, -1));
-        // return solveUsingRec(0, 1, prices, dp);
-        return solveUsingSpaceOptimized(prices);
+        return solveSpaceOptimized1(prices);
     }
 };
