cff3e7b3754778b66221b02d5921bcb29a831732
Prakhar Maheshwari <113543305+prakhar7017@users.noreply.github.com>
2025-08-24 22:52:00 +0530

    Time: 1 ms (49.06%) | Memory: 16.4 MB (13.44%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,24 +1,47 @@
 class Solution {
 public:
-    int solveUsingRec(int index, int buy, vector<int>& prices,vector<vector<int>>& dp) {
+    int solveUsingRec(int index, int buy, vector<int>& prices,
+                      vector<vector<int>>& dp) {
         if (index >= prices.size())
             return 0;
 
-        if(dp[index][buy]!=-1) return dp[index][buy];    
+        if (dp[index][buy] != -1)
+            return dp[index][buy];
 
         int profit = 0;
         if (buy) {
-            profit = max(-prices[index] + solveUsingRec(index + 1, 0, prices,dp),
-                         0 + solveUsingRec(index + 1, 1, prices,dp));
+            profit =
+                max(-prices[index] + solveUsingRec(index + 1, 0, prices, dp),
+                    0 + solveUsingRec(index + 1, 1, prices, dp));
         } else {
-            profit = max(prices[index] + solveUsingRec(index + 2, 1, prices,dp),
-                         0 + solveUsingRec(index + 1, 0, prices,dp));
+            profit =
+                max(prices[index] + solveUsingRec(index + 2, 1, prices, dp),
+                    0 + solveUsingRec(index + 1, 0, prices, dp));
         }
-        return dp[index][buy]=profit;
+        return dp[index][buy] = profit;
+    }
+
+    int solveUsingTab(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<int>> dp(n + 1, vector<int>(2, 0));
+        int profit = 0;
+        for (int index = n - 1; index >= 0; index--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                if (buy) {
+                    profit = max(-prices[index] + dp[index + 1][0],
+                                 0 + dp[index + 1][1]);
+                } else {
+                    profit = max(prices[index] + dp[index + 2][1],
+                                 0 + dp[index + 1][0]);
+                }
+                dp[index][buy]=profit;
+            }
+        }
+        return dp[0][1];
     }
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
         vector<vector<int>> dp(n, vector<int>(2, -1));
-        return solveUsingRec(0,1,prices,dp);
+        return solveUsingRec(0, 1, prices, dp);
     }
 };
