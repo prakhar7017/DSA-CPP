db487217b2e5f5201aa2ddfbf7ee789f6b30e037
Prakhar Maheshwari <113543305+prakhar7017@users.noreply.github.com>
2025-08-24 23:09:25 +0530

    Time: 107 ms (36.76%) | Memory: 95 MB (46.89%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int solveUsingRec(int index, int buy,int fee, vector<int>& prices,
+    int solveUsingRec(int index, int buy, int fee, vector<int>& prices,
                       vector<vector<int>>& dp) {
         if (index >= prices.size())
             return 0;
@@ -10,19 +10,59 @@ public:
 
         int profit = 0;
         if (buy) {
-            profit =
-                max(-prices[index] + solveUsingRec(index + 1, 0,fee, prices, dp),
-                    0 + solveUsingRec(index + 1, 1,fee, prices, dp));
+            profit = max(-prices[index] +
+                             solveUsingRec(index + 1, 0, fee, prices, dp),
+                         0 + solveUsingRec(index + 1, 1, fee, prices, dp));
         } else {
-            profit =
-                max(-fee+prices[index] + solveUsingRec(index + 1, 1,fee, prices, dp),
-                    0 + solveUsingRec(index + 1, 0,fee, prices, dp));
+            profit = max(-fee + prices[index] +
+                             solveUsingRec(index + 1, 1, fee, prices, dp),
+                         0 + solveUsingRec(index + 1, 0, fee, prices, dp));
         }
         return dp[index][buy] = profit;
     }
+
+    int solveUsingTab(int fee, vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<int>> dp(n+1, vector<int>(2, 0));
+        int profit = 0;
+        for (int index = n - 1; index >= 0; index--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                if (buy) {
+                    profit = max(-prices[index] + dp[index + 1][0],
+                                 0 + dp[index + 1][1]);
+                } else {
+                    profit = max(-fee + prices[index] + dp[index + 1][1],
+                                 0 + dp[index + 1][0]);
+                }
+                dp[index][buy]=profit;
+            }
+        }
+        return dp[0][1];
+    }
+
+    int solveUsingSpaceOptmizes(int fee, vector<int>& prices) {
+        int n = prices.size();
+        vector<int>next(2, 0);
+        vector<int>curr(2, 0);
+        int profit = 0;
+        for (int index = n - 1; index >= 0; index--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                if (buy) {
+                    profit = max(-prices[index] + next[0],
+                                 0 + next[1]);
+                } else {
+                    profit = max(-fee + prices[index] + next[1],
+                                 0 + next[0]);
+                }
+                curr[buy]=profit;
+            }
+            next=curr;
+        }
+        return next[1];
+    }
     int maxProfit(vector<int>& prices, int fee) {
-        int n=prices.size();
-        vector<vector<int>>dp(n,vector<int>(2,-1));
-        return solveUsingRec(0,1,fee,prices,dp);
+        int n = prices.size();
+        vector<vector<int>> dp(n, vector<int>(2, -1));
+        return solveUsingSpaceOptmizes(fee,prices);
     }
 };
