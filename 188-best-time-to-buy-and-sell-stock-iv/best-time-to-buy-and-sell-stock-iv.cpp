1c6da53098156357382710c28e8286967d4954a0
Prakhar Maheshwari <113543305+prakhar7017@users.noreply.github.com>
2025-08-24 20:50:02 +0530

    Time: 7 ms (63.68%) | Memory: 18.4 MB (17.60%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -41,12 +41,35 @@ public:
         }
         return dp[0][1][k];
     }
+
+    int solveUsingSpace(int k, vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<int>>next(2, vector<int>(k + 1, 0));
+        vector<vector<int>>curr(2, vector<int>(k + 1, 0));
+        int profit;
+        for (int index = n - 1; index >= 0; index--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                for (int cap = 1; cap <= k; cap++) {
+                    if (buy) {
+                        profit = max(-prices[index] + next[0][cap],
+                                     0 + next[1][cap]);
+                    } else {
+                        profit = max(prices[index] + next[1][cap - 1],
+                                     0 + next[0][cap]);
+                    }
+                    curr[buy][cap]=profit;
+                }
+            }
+            next=curr;
+        }
+        return next[1][k];
+    }
     int maxProfit(int k, vector<int>& prices) {
         int n = prices.size();
         vector<vector<vector<int>>> dp(
             n, vector<vector<int>>(2, vector<int>(k + 1, -1)));
 
         // return solveUsingRec(0, 1, k, prices, dp);
-        return solveUsingTab(k, prices);
+        return solveUsingSpace(k, prices);
     }
 };
