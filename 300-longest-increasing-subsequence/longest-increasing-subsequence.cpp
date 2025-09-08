5388bbeeef5b89bfe71ec01a293b224cba903ab1
Prakhar Maheshwari <113543305+prakhar7017@users.noreply.github.com>
2025-09-09 01:32:16 +0530

    Time: 93 ms (48.12%) | Memory: 296.6 MB (13.44%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -33,19 +33,16 @@ public:
     }
 
     int solveUsingBinarySearch(vector<int>& nums){
-        int n= nums.size();
+        int n=nums.size();
         vector<int>sorted;
-
         for(int i=0;i<n;i++){
-            auto it= lower_bound(begin(sorted),end(sorted),nums[i]);
-
-            if(it==end(sorted)){
+            auto it=lower_bound(sorted.begin(),sorted.end(),nums[i]);
+            if(it==sorted.end()){
                 sorted.push_back(nums[i]);
             }else{
-                *it = nums[i];
+                *it=nums[i];
             }
         }
-
         return sorted.size();
     }
     int lengthOfLIS(vector<int>& nums) {
