611cc859060a5182981567401c4936d118b4aebb
Prakhar Maheshwari <113543305+prakhar7017@users.noreply.github.com>
2025-10-03 00:16:18 +0530

    Time: 3 ms (8.35%) | Memory: 8.9 MB (31.14%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -12,7 +12,32 @@ public:
         }
         return st.empty();
     }
+
+     bool solveUsingStack(string& s){
+        stack<char>st;
+
+        for(char &ch:s){
+            if(st.empty() || ch=='(' || ch=='{' || ch=='['){
+                st.push(ch);
+                continue;
+            }
+
+            if(ch==')'){
+                if(st.top()=='(') st.pop();
+                else return false;
+            }else if(ch=='}'){
+                if(st.top()=='{') st.pop();
+                else return false;
+            }else if(ch==']'){
+                if(st.top()=='[') st.pop();
+                else return false;
+            }else{
+                return false;
+            }
+        }
+        return st.empty();
+    }
     bool isValid(string s) {
-        return solveUsingMoreCleanApproach(s);
+        return solveUsingStack(s);
     }
 };
