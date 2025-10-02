66b98d92ac8d692bb77546eae680dd5bae8c9e26
Prakhar Maheshwari <113543305+prakhar7017@users.noreply.github.com>
2025-10-02 23:53:27 +0530

    Time: 25 ms (27.55%) | Memory: 27.3 MB (23.92%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -14,7 +14,40 @@ public:
         }
         return ans;
     }
+
+    string generate(string& s) {
+        int n = s.size();
+        int arr[26] = {0};
+
+        for (char& ch : s) {
+            arr[ch - 'a']++;
+        }
+
+        string newWord = "";
+        for (int i = 0; i < 26; i++) {
+            int freq = arr[i];
+            if (freq > 0) {
+                newWord += string(freq, i + 'a');
+            }
+        }
+        return newWord;
+    }
+    vector<vector<string>> solveUsingWithoutSort(vector<string>& strs) {
+        int n = strs.size();
+        unordered_map<string, vector<string>> mp;
+        vector<vector<string>> ans;
+        for (int i = 0; i < n; i++) {
+            string word = strs[i];
+            string new_word = generate(word);
+            mp[new_word].push_back(word);
+        }
+        for (auto it : mp) {
+            ans.push_back(it.second);
+        }
+        return ans;
+    }
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        return solveUsingSort(strs);
+        // return solveUsingSort(strs);
+        return solveUsingWithoutSort(strs);
     }
 };
