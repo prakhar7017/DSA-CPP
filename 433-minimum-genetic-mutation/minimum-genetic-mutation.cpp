c4a511f76ad9eed2242c9454eee03fc469219f0f
Prakhar Maheshwari <113543305+prakhar7017@users.noreply.github.com>
2025-12-14 03:00:27 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (45.09%) - LeetSync

--- 433-minimum-genetic-mutation/minimum-genetic-mutation.cpp
+++ 433-minimum-genetic-mutation/minimum-genetic-mutation.cpp
@@ -1,23 +1,30 @@
 class Solution {
 public:
-    int solveUsingBFS(string startGene, string endGene, vector<string>& bank){
-        unordered_set<string>bankset(bank.begin(),bank.end());
-        unordered_set<string>vis;
-        queue<string>q;
-        int level=0;
+    int minMutation(string startGene, string endGene, vector<string>& bank) {
+        unordered_set<string> bankset(begin(bank), end(bank));
+        unordered_set<string> vis;
+
+        if(bankset.find(endGene)==bankset.end()) return -1;
+
+        queue<string> q;
+        int level = 0;
         q.push(startGene);
         vis.insert(startGene);
-        while(!q.empty()){
-            int size=q.size();
-            while(size--){
-                string currStr=q.front();
+
+        while (!q.empty()) {
+            int levelSize = q.size();
+            while (levelSize--) {
+                string currStr = q.front();
                 q.pop();
-                if(currStr==endGene) return level;
-                for(char ch:"ACGT"){
-                    for(int i=0;i<currStr.length();i++){
-                        string neigh=currStr;
-                        neigh[i]=ch;
-                        if(vis.find(neigh)==vis.end() && bankset.find(neigh)!=bankset.end()){
+                if (currStr == endGene)
+                    return level;
+
+                for (char ch : "ACGT") {
+                    for (int i = 0; i < currStr.length(); i++) {
+                        string neigh = currStr;
+                        neigh[i] = ch;
+                        if (vis.find(neigh) == vis.end() &&
+                            bankset.find(neigh) != bankset.end()) {
                             q.push(neigh);
                             vis.insert(neigh);
                         }
@@ -26,10 +33,7 @@ public:
             }
             level++;
         }
+
         return -1;
-    }
-    int minMutation(string startGene, string endGene, vector<string>& bank) {
-       return solveUsingBFS(startGene,endGene,bank);
- 
     }
 };
