43e8b6e89f8c76ff800f8c5115d94ecbcc66c4db
Prakhar Maheshwari <113543305+prakhar7017@users.noreply.github.com>
2025-12-27 00:03:24 +0530

    Time: 7 ms (23.24%) | Memory: 12 MB (61.63%) - LeetSync

--- 133-clone-graph/clone-graph.cpp
+++ 133-clone-graph/clone-graph.cpp
@@ -14,13 +14,35 @@ public:
         }
     }
 
+    void BFS(queue<Node*>&q){
+        while(!q.empty()){
+            Node* currNode = q.front();
+            Node* clonedNode = mp[currNode];
+            q.pop();
+
+            for(Node* neighNode: currNode->neighbors){
+                if(mp.find(neighNode)==mp.end()){
+                    Node* clonedNeighNode = new Node(neighNode->val);
+                    mp[neighNode]=clonedNeighNode;
+                    clonedNode->neighbors.push_back(clonedNeighNode);
+                    q.push(neighNode);
+                }else{
+                    clonedNode->neighbors.push_back(mp[neighNode]);
+                }
+            }
+        }
+    }
+
     Node* cloneGraph(Node* node) {
         if (!node)
             return nullptr;
         mp.clear();
         Node* clonedNode = new Node(node->val);
         mp[node] = clonedNode;
-        DFS(node, clonedNode);
+        // DFS(node, clonedNode);
+        queue<Node*> que;
+        que.push(node);
+        BFS(que);
         return clonedNode;
     }
 };
