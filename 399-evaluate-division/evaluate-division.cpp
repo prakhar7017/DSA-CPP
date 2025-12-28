fa932614a207258c82decd532b3529d3821b0c80
Prakhar Maheshwari <113543305+prakhar7017@users.noreply.github.com>
2025-12-28 21:36:35 +0530

    Time: 2 ms (24.00%) | Memory: 12.6 MB (6.42%) - LeetSync

---
+++ 399-evaluate-division/evaluate-division.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    typedef pair<string,double>P;
+    void dfs(string src,string dest,unordered_set<string>&vis,double &result,double product,unordered_map<string,vector<P>>&adj){
+        if(vis.find(src)!=vis.end()) return ;
+        vis.insert(src);
+
+        if(src == dest) {
+            result = product;
+            return;
+        }
+
+        for(auto p:adj[src]){
+            string v = p.first;
+            double val = p.second;
+            dfs(v,dest,vis,result,product*val,adj);
+        }    
+    }
+    vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
+        int n = equations.size();
+        unordered_map<string,vector<P>>adj;
+
+        for(int i=0;i<n;i++){
+            string u = equations[i][0];
+            string v = equations[i][1];
+            double val = values[i];
+
+            adj[u].push_back({v,val});
+            adj[v].push_back({u,1.0/val});
+        }
+        vector<double>ans;
+        for(vector<string>& query:queries){
+            string src = query[0];
+            string dest = query[1];
+            double result = -1.0;
+            double product = 1.0;
+
+            if(adj.find(src)!=adj.end()){
+                unordered_set<string>vis;
+                dfs(src,dest,vis,result,product,adj);
+            }
+
+            ans.push_back(result);
+        }
+        return ans;
+    }
+};
