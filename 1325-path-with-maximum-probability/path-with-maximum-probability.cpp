fb9d0795c369fc875485a7d9403e4c00db63c06c
Prakhar Maheshwari <113543305+prakhar7017@users.noreply.github.com>
2025-12-29 21:19:45 +0530

    Time: 69 ms (20.89%) | Memory: 74.9 MB (14.56%) - LeetSync

--- 1325-path-with-maximum-probability/path-with-maximum-probability.cpp
+++ 1325-path-with-maximum-probability/path-with-maximum-probability.cpp
@@ -1,36 +1,41 @@
 class Solution {
 public:
-    
-    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end) {
-        vector<pair<int,double>> graph[n];
+    typedef pair<double,int>P;
+    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start_node, int end_node) {
+        unordered_map<int,vector<pair<int,double>>>adj;
         for(int i=0;i<edges.size();i++){
-            graph[edges[i][0]].push_back({edges[i][1],succProb[i]});
-            graph[edges[i][1]].push_back({edges[i][0],succProb[i]});
+            int u = edges[i][0];
+            int v = edges[i][1];
+            double prob = succProb[i];
+            adj[u].push_back({v,prob});
+            adj[v].push_back({u,prob});
         }
- 
-          vector<int> vis(n,0);
-          priority_queue<pair<double,int>> pq;
-          vector<double> dist(n,0);
-          pq.push({1.00,start});
+        priority_queue<P>q;
+        vector<double>prob(n,0.0);
+        vector<bool>vis(n,false);
+        q.push({1.0,start_node});
+        prob[start_node]=1.0;
 
-           while(!pq.empty()){
-               auto top=pq.top();
-               pq.pop();
-               int node=top.second;
-               double currdist=top.first;
-                vis[node]=1;
-               for(auto x:graph[node]){
-                   int child= x.first;
-                   double pathprob=x.second;
+        while(!q.empty()){
+            double currProb = q.top().first;
+            int currNode = q.top().second;
+            q.pop();
+            if(vis[currNode]) continue;
+            vis[currNode]=true;
+            for(auto &it:adj[currNode]){
+                int neighNode = it.first;
+                double neighProb = it.second;
 
-                   if(vis[child]==0){
-                        if(currdist*pathprob>dist[child]){
-                            dist[child]= currdist*pathprob;
-                            pq.push({dist[child],child});
-                        }
-                   }
-               }
-           }
-           return dist[end];
+                if(!vis[neighNode]){
+                    if(currProb*neighProb>prob[neighNode]){
+                        prob[neighNode]=currProb*neighProb;
+                        q.push({prob[neighNode],neighNode});
+
+                    }
+                }
+            }
+        }
+
+        return prob[end_node];
     }
 };
