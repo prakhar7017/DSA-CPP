2531109c721d72d221b7d429933e9cd1f7554413
Prakhar Maheshwari <113543305+prakhar7017@users.noreply.github.com>
2026-01-02 16:17:55 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (80.22%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,25 +1,39 @@
 class Solution:
-    def search(self, nums: List[int], target: int) -> int:
-        left, right = 0, len(nums) - 1
-        
-        while left <= right:
-            mid = left + (right - left) // 2
-            
-            if nums[mid] == target:
-                return mid
-            
-            # Identify which half is sorted
-            if nums[left] <= nums[mid]:
-                # Left half is sorted
-                if nums[left] <= target < nums[mid]:
-                    right = mid - 1 # Target is in the sorted left half
-                else:
-                    left = mid + 1  # Target is in the right half
+    def findMinimum(self,nums:List[int]):
+        n = len(nums)
+        left = 0
+        right = n-1
+
+        while(left<right):
+            mid = left+(right-left)//2
+            if nums[mid]>nums[right]:
+                left = mid + 1
             else:
-                # Right half is sorted
-                if nums[mid] < target <= nums[right]:
-                    left = mid + 1  # Target is in the sorted right half
-                else:
-                    right = mid - 1 # Target is in the left half
-                    
-        return -1
+                right = mid
+
+        return right    
+
+    def BinarySearch(self,left,right,target,nums):
+        while(left<=right):
+            mid = left+(right-left)//2
+            if nums[mid]==target:
+                return mid
+            elif nums[mid]<target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return -1                        
+
+
+    def search(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        pivot_index = self.findMinimum(nums)
+
+        idx = self.BinarySearch(0,pivot_index-1,target,nums)
+
+        if idx != -1:
+            return idx
+
+        idx = self.BinarySearch(pivot_index,n-1,target,nums)
+        return idx 
