b2ab4ed9166dd5eae81a0156df220ecbadd52240
Prakhar Maheshwari <113543305+prakhar7017@users.noreply.github.com>
2026-01-08 01:12:11 +0530

    Time: 3 ms (40.25%) | Memory: 19.5 MB (63.12%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,25 +1,47 @@
 class Solution {
 public:
-    void checkDFS(int city,vector<bool>&vis,vector<vector<int>>& isConnected){
-        vis[city]=true;
+    vector<int>parent;
+    vector<int>rank;
+    int components;
 
-        for(int neighCity=0;neighCity<isConnected.size();neighCity++){
-            if(isConnected[city][neighCity] && !vis[neighCity]){
-                checkDFS(neighCity,vis,isConnected);
-            }
-        }
+    int findParent(int x){
+        if(parent[x]==x) return x;
+        return parent[x]=findParent(parent[x]);
     }
-    int findCircleNum(vector<vector<int>>& isConnected) {
-        int cities = isConnected.size();
-        vector<bool>vis(cities,false);
-        int provinces = 0;
 
-        for (int city = 0; city <cities; city++) {
-            if(!vis[city]){
-                checkDFS(city,vis,isConnected);
-                provinces++;
+    void Union(int x,int y){
+        x = findParent(x);
+        y = findParent(y);
+
+        if(x==y) return;
+
+        if(rank[x]<rank[y]){
+            parent[x]=y;
+        }else if(rank[x]>rank[y]){
+            parent[y]=x;
+        }else{
+            parent[y]=x;
+            rank[x]++;
+        }
+        components--;
+    }
+
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        components = n;
+
+        parent.resize(n);
+        rank.resize(n,0);
+
+        iota(begin(parent),end(parent),0);
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(isConnected[i][j]==1){
+                    Union(i,j);
+                }
             }
         }
-        return provinces;
+        return components;
     }
 };
