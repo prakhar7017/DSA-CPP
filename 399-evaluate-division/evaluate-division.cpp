b0ac1cea02bf741b148beb0e892a7b7836af648f
Prakhar Maheshwari <113543305+prakhar7017@users.noreply.github.com>
2026-01-18 03:05:35 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (55.78%) - LeetSync

--- 399-evaluate-division/evaluate-division.cpp
+++ 399-evaluate-division/evaluate-division.cpp
@@ -1,40 +1,38 @@
 class Solution {
 public:
     typedef pair<string,double>P;
-    void dfs(string src,string dest,unordered_set<string>&vis,double &result,double product,unordered_map<string,vector<P>>&adj){
-        if(vis.find(src)!=vis.end()) return ;
-        vis.insert(src);
 
-        if(src == dest) {
+    void DFS(string src, string dest,unordered_set<string>&vis,double &result,double product,unordered_map<string,vector<P>>&adj){
+        if(vis.find(src)!=vis.end()) return;
+        vis.insert(src);
+        
+        if(src==dest){
             result = product;
             return;
         }
 
-        for(auto p:adj[src]){
-            string v = p.first;
-            double val = p.second;
-            dfs(v,dest,vis,result,product*val,adj);
-        }    
+        for(auto [v,val]:adj[src]){
+            DFS(v,dest,vis,result,product*val,adj);
+        }
     }
-    void BFS(string src,string dest,unordered_set<string>&vis,double &result,double product,unordered_map<string,vector<P>>&adj){
+
+    void BFS(string src, string dest,unordered_set<string>&vis,double &result,double product,unordered_map<string,vector<P>>&adj){
         queue<P>q;
         q.push({src,product});
         vis.insert(src);
 
         while(!q.empty()){
-            auto [node,prod] = q.front();
+            auto [curr, curr_prod] = q.front();
             q.pop();
 
-            if(node == dest) {
-                result = prod;
+            if(curr == dest) {
+                result = curr_prod;
                 return;
             }
 
-            for(auto p:adj[node]){
-                string v = p.first;
-                double val = p.second;
+            for(auto [v,val]:adj[curr]){
                 if(vis.find(v)==vis.end()){
-                    q.push({v,prod*val});
+                    q.push({v,val*curr_prod});
                     vis.insert(v);
                 }
             }
@@ -47,26 +45,27 @@ public:
         for(int i=0;i<n;i++){
             string u = equations[i][0];
             string v = equations[i][1];
-            double val = values[i];
-
-            adj[u].push_back({v,val});
-            adj[v].push_back({u,1.0/val});
+            double value = values[i];
+            adj[u].push_back({v,value});
+            adj[v].push_back({u,1.0/value});
         }
         vector<double>ans;
-        for(vector<string>& query:queries){
+
+        for(vector<string>&query:queries){
             string src = query[0];
             string dest = query[1];
-            double result = -1.0;
             double product = 1.0;
+            double result = -1.0;
 
-            if(adj.find(src)!=adj.end() && adj.find(dest)!=adj.end()){
+            if(adj.find(src)!=adj.end() && adj.find(dest)!=adj.end()) {
                 unordered_set<string>vis;
-                // dfs(src,dest,vis,result,product,adj);
+                // DFS(src,dest,vis,result,product,adj);
                 BFS(src,dest,vis,result,product,adj);
             }
 
             ans.push_back(result);
         }
+
         return ans;
     }
 };
