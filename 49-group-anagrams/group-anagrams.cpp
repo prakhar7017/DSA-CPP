05a93e285e429d4c5670ea7c6c97ab11d351bda9
Prakhar Maheshwari <113543305+prakhar7017@users.noreply.github.com>
2026-06-13 17:17:27 +0530

    Time: 14 ms (74.36%) | Memory: 25.8 MB (58.84%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,53 +1,29 @@
 class Solution {
 public:
-    vector<vector<string>> solveUsingSort(vector<string>& strs) {
-        int n = strs.size();
-        unordered_map<string, vector<string>> mp;
-        vector<vector<string>> ans;
-        for (int i = 0; i < n; i++) {
-            string word = strs[i];
-            sort(begin(word), end(word));
-            mp[word].push_back(strs[i]);
+    string generate(string &str){
+        int arr[26]={0};
+        for(char &c:str){
+            arr[c-'a']++;
         }
-        for (auto it : mp) {
-            ans.push_back(it.second);
-        }
-        return ans;
-    }
-
-    string generate(string& s) {
-        int n = s.size();
-        int arr[26] = {0};
-
-        for (char& ch : s) {
-            arr[ch - 'a']++;
-        }
-
         string newWord = "";
-        for (int i = 0; i < 26; i++) {
+        for(int i=0;i<26;i++){
             int freq = arr[i];
-            if (freq > 0) {
-                newWord += string(freq, i + 'a');
+            if(freq>0){
+                newWord+= string(freq,i+'a');
             }
         }
         return newWord;
     }
-    vector<vector<string>> solveUsingWithoutSort(vector<string>& strs) {
-        int n = strs.size();
-        unordered_map<string, vector<string>> mp;
-        vector<vector<string>> ans;
-        for (int i = 0; i < n; i++) {
-            string word = strs[i];
-            string new_word = generate(word);
-            mp[new_word].push_back(word);
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>>mp;
+        for(string &str:strs){
+            string anagram = generate(str);
+            mp[anagram].push_back(str);
         }
-        for (auto it : mp) {
+        vector<vector<string>>ans;
+        for(auto &it:mp){
             ans.push_back(it.second);
         }
         return ans;
-    }
-    vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        // return solveUsingSort(strs);
-        return solveUsingWithoutSort(strs);
     }
 };
