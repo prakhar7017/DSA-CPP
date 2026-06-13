00cf9bba156686de47e234aa5bdc984e09c32d98
Prakhar Maheshwari <113543305+prakhar7017@users.noreply.github.com>
2026-06-13 17:52:04 +0530

    Time: 2 ms (57.85%) | Memory: 19.8 MB (17.11%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -6,14 +6,25 @@ public:
         for(int num:nums){
             mp[num]++;
         }
-        vector<P>items(begin(mp),end(mp));
-        sort(begin(items),end(items),[](P& a, P& b){
-            return a.second > b.second;
-        });
-        vector<int>ans;
-        for(int i=0;i<k;i++){
-            ans.push_back(items[i].first);
+        // vector<P>items(begin(mp),end(mp));
+        // // sort(begin(items),end(items),[](P& a, P& b){
+        // //     return a.second > b.second;
+        // // });
+        vector<vector<int>>buckets(nums.size()+1);
+        for(auto& [num,freq]: mp){
+            buckets[freq].push_back(num);
         }
+        vector<int>ans;
+        for(int freq= buckets.size()-1;freq>=1;freq--){
+            for(int num:buckets[freq]){
+                ans.push_back(num);
+                if(ans.size()==k) return ans;   
+            }
+        }
+        // vector<int>ans;
+        // for(int i=0;i<k;i++){
+        //     ans.push_back(items[i].first);
+        // }
         return ans;
     }
 };
