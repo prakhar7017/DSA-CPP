b05653f49d229075197e0b2a2ce36fb53c9ce362
Prakhar Maheshwari <113543305+prakhar7017@users.noreply.github.com>
2026-07-04 14:04:45 +0530

    Time: 5 ms (7.39%) | Memory: 10.2 MB (32.23%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,23 +1,38 @@
 class Solution {
 public:
-    bool isPalindrome(string str) {
+    bool bruteForce(string& str) {
         string newStr = "";
 
-        for(char ch:str){
-            if(isalnum(ch)){
+        for (char ch : str) {
+            if (isalnum(ch)) {
                 newStr.push_back(tolower(ch));
             }
         }
-        if(newStr.size() == 0) return true;
+        if (newStr.size() == 0)
+            return true;
 
         int s = 0;
-        int e = newStr.size()-1;
+        int e = newStr.size() - 1;
 
-        while(s<e){
-            if(newStr[s++]!=newStr[e--]){
+        while (s < e) {
+            if (newStr[s++] != newStr[e--]) {
                 return false;
             }
         }
         return true;
     }
+    bool optimised(string str){
+        int n = str.size();
+        int s = 0;
+        int e = n-1;
+
+        while(s<e){
+            while(s<e && !isalnum(str[s])) s++;
+            while(s<e && !isalnum(str[e])) e--;
+            if(tolower(str[s])!=tolower(str[e])) return false;
+            s++;e--;
+        }
+        return true;
+    }
+    bool isPalindrome(string str) { return optimised(str);}
 };
