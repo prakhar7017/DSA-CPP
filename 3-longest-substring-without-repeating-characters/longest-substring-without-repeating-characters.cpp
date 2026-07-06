e9006801a7ac5846322202ed18d24cb94a9d475c
Prakhar Maheshwari <113543305+prakhar7017@users.noreply.github.com>
2026-07-06 19:26:11 +0530

    Time: 12 ms (42.03%) | Memory: 11.8 MB (65.98%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,19 +1,43 @@
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-        unordered_map<char,int>lastSeen;
-        int n=s.length();
-        int l=0;
-        int maxLen=0;
+    bool checkDuplicate(int i, int j, string& s) {
+        vector<int> freq(256, 0);
 
-        for(int r=0;r<n;r++){
-            char ch = s[r];
-            if(lastSeen.find(ch)!=lastSeen.end() && lastSeen[ch]>=l){
-                l = lastSeen[ch] + 1;
+        for (int k = i; k <= j; k++) {
+            unsigned char c = s[k];
+            if (freq[c])
+                return false;
+            freq[c] = 1;
+        }
+        return true;
+    }
+    int solveBruteForce(string s) {
+        int maxLen = 0;
+        int n = s.size();
+        for (int i = 0; i < n; i++) {
+            char ch = s[i];
+            for (int j = i; j < n; j++) {
+                if (checkDuplicate(i, j, s)) {
+                    maxLen = max(maxLen, j - i + 1);
+                }
             }
-            maxLen = max(r-l+1,maxLen);
-            lastSeen[ch]=r;
         }
         return maxLen;
     }
+    int optimised(string& s) {
+        int n = s.size();
+        int l = 0;
+        int ans = 0;
+        unordered_map<char, int> mp;
+        for (int r = 0; r < n; r++) {
+            char ch = s[r];
+            if(mp.find(ch)!=mp.end() && mp[ch]>=l){
+                l=mp[ch]+1;
+            }
+            mp[ch]=r;
+            ans = max(ans,r-l+1);
+        }
+        return ans;
+    }
+    int lengthOfLongestSubstring(string s) { return optimised(s); }
 };
