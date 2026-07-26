0296bc3f67a21d8557a92bf1db10cc3d3dca5ad3
Prakhar Maheshwari <113543305+prakhar7017@users.noreply.github.com>
2026-07-26 22:00:22 +0530

    Time: 1 ms (37.14%) | Memory: 95 MB (80.06%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -80,7 +80,39 @@ public:
 
         return (element2 + element1) / 2.0;
     }
+    double optimisedBinarySearch(vector<int>& nums1, vector<int>& nums2){
+        if(nums1.size()>nums2.size()) return optimisedBinarySearch(nums2,nums1);
+
+        int m = nums1.size();
+        int n = nums2.size();
+
+        int s = 0;
+        int e = m;
+
+        while(s<=e){
+            int Px = s + (e-s)/2;
+            int Py = (m+n+1)/2 - Px;
+
+            // left half
+            int x1 = (Px == 0) ? INT_MIN: nums1[Px-1];
+            int x2 = (Py == 0) ? INT_MIN: nums2[Py-1];
+            int x3 = (Px == m) ? INT_MAX: nums1[Px];
+            int x4 = (Py == n) ? INT_MAX: nums2[Py];
+
+            if(x1<=x4 && x2<=x3) {
+                if((m+n)%2 == 1) return max(x1,x2);
+                return (max(x1,x2) + min(x3,x4))/2.0;
+            }
+            
+            if(x1>x4){
+                e = Px - 1;
+            }else{
+                s = Px + 1;
+            }
+        }
+        return -1;
+    }
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        return bruteForceWithNoExtraSpace(nums1,nums2);
+        return optimisedBinarySearch(nums1,nums2);
     }
 };
