be39550046bf661e629e4d679cdc39f201781113
Prakhar Maheshwari <113543305+prakhar7017@users.noreply.github.com>
2026-08-09 02:30:58 +0530

    Time: 0 ms (100.00%) | Memory: 22.9 MB (53.66%) - LeetSync

--- 143-reorder-list/reorder-list.cpp
+++ 143-reorder-list/reorder-list.cpp
@@ -35,7 +35,77 @@ public:
         }
         return;
     }
+
+    void reorderListArray(ListNode* head){
+        if(!head || !head->next) return;
+        vector<ListNode*>nodes;
+
+        ListNode* curr = head;
+
+        while(curr){
+            nodes.push_back(curr);
+            curr = curr->next;
+        }
+
+        int s = 0;
+        int e = nodes.size()-1;
+
+        while(s<e){
+            nodes[s]->next = nodes[e];
+            s++;
+            if(s==e) break;
+            nodes[e]->next = nodes[s];
+            e--;
+        }
+
+        nodes[s]->next=nullptr;
+    }
+
+    ListNode* findMiddle(ListNode* head){
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast && fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+    ListNode* reverseL(ListNode* head){
+        if(!head) return head;
+        ListNode* curr = head;
+        ListNode* prev = nullptr;
+        while(curr){
+            ListNode* next = curr -> next;
+            curr -> next = prev;
+            prev =  curr;
+            curr = next;
+
+        }
+        return prev;
+    }
+    void merge(ListNode* h1,ListNode* h2){
+        while(h2){
+            ListNode* n1 = h1->next;
+            ListNode* n2 = h2->next;
+            h1->next = h2;
+            h2->next = n1;
+
+            h1 =  n1;
+            h2 =  n2;
+        }
+    }
     void reorderList(ListNode* head) {
-        bruteForce(head);
+        // reorderListArray(head);
+        if(!head || !head->next) return;
+
+        ListNode* middle = findMiddle(head);
+
+        ListNode* second = middle->next;
+        middle->next = nullptr;
+
+        ListNode* reverse = reverseL(second);
+        
+        merge(head,reverse);
     }
 };
