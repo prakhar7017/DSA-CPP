75b6debe9c9ae193b0ce353725edfc17f3012006
Prakhar Maheshwari <113543305+prakhar7017@users.noreply.github.com>
2026-08-19 22:05:29 +0530

    Time: 0 ms (100.00%) | Memory: 22.8 MB (98.38%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -12,6 +12,16 @@
 class Solution {
 public:
     typedef pair<bool,int>P;
+    bool _isBalanced=true;
+    int height(TreeNode* root){
+        if(!root) return 0;
+        int leftAns = height(root->left);
+        int rightAns = height(root->right);
+
+        if(_isBalanced && abs(leftAns-rightAns)>1) _isBalanced = false;
+        return max(leftAns,rightAns)+1;
+    }
+
     P checkBalance(TreeNode* root){
         if(!root) return {true,0};
         P leftAns = checkBalance(root->left);
@@ -22,5 +32,13 @@ public:
         }
         return {false,height};
     }
-    bool isBalanced(TreeNode* root) { return checkBalance(root).first;}
+    bool isBalanced(TreeNode* root) { 
+        // return checkBalance(root).first;
+        if(!root) return true;
+        int leftAns = height(root->left);
+        int rightAns = height(root->right);
+
+        if(_isBalanced && abs(leftAns-rightAns)>1) _isBalanced = false;
+        return _isBalanced;
+    }
 };
