960fbe398fe73ae72e075d3db4563ab896171846
Prakhar Maheshwari <113543305+prakhar7017@users.noreply.github.com>
2026-09-12 23:20:40 +0530

    Time: 4 ms (30.79%) | Memory: 13.4 MB (72.22%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,54 +1,31 @@
 class Solution {
 public:
-    bool isValid(string& s) {
+    vector<string>ans;
+    bool isValid(string &curr){
         int count = 0;
-        for (char& ch : s) {
-            if (ch == '(')
-                count++;
-            else if (ch == ')')
-                count--;
-            if (count < 0)
-                return false;
+        for(char ch:curr){
+            if(ch == '(') count++;
+            else count--;
+            if(count < 0) return false;
         }
         return count == 0;
     }
-    void solveUsingRecursion(string& currString, vector<string>& result,
-                             int& n) {
-        if (currString.length() == 2 * n) {
-            if (isValid(currString))
-                result.push_back(currString);
+    void solve(string&curr,int n){
+        if(curr.length()==2*n){
+            if(isValid(curr)) ans.push_back(curr);
             return;
         }
-        currString.push_back('(');
-        solveUsingRecursion(currString, result, n);
-        currString.pop_back();
-        currString.push_back(')');
-        solveUsingRecursion(currString, result, n);
-        currString.pop_back();
-    }
-
-    void solveUsingRecursionOptimised(string currString, vector<string>& result,
-                                      int n, int open, int close) {
-        if (currString.length() == 2 * n) {
-            result.push_back(currString);
-            return;
-        }
-
-        if (open < n) {
-            solveUsingRecursionOptimised(currString + "(", result, n, open + 1,
-                                         close);
-        }
-
-        if (close < open) {
-            solveUsingRecursionOptimised(currString + ")", result, n, open,
-                                         close + 1);
-        }
+        curr.push_back('(');
+        solve(curr,n);
+        //backstrack
+        curr.pop_back();
+        curr.push_back(')');
+        solve(curr,n);
+        curr.pop_back();
     }
     vector<string> generateParenthesis(int n) {
-        vector<string> result;
-        string currString = "";
-        // solveUsingRecursion(currString, result, n, 0, 0);
-        solveUsingRecursionOptimised(currString, result, n, 0, 0);
-        return result;
+        string curr="";
+        solve(curr,n);
+        return ans;
     }
-}; // (()) ((((
+};
