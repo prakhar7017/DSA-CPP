2d7a247159efd0efab36cad022859e753835fbee
Prakhar Maheshwari <113543305+prakhar7017@users.noreply.github.com>
2026-09-12 23:23:53 +0530

    Time: 6 ms (12.50%) | Memory: 13.4 MB (69.38%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -23,9 +23,28 @@ public:
         solve(curr,n);
         curr.pop_back();
     }
+    void solve2(string&curr,int n,int open,int close){
+        if(curr.length()==2*n){
+            ans.push_back(curr);
+            return;
+        }
+        if(open<n){
+            curr.push_back('(');
+            solve2(curr,n,open+1,close);
+            curr.pop_back(); 
+        }
+
+        if(close<open){
+            curr.push_back(')');
+            solve(curr,n);
+            curr.pop_back();
+        }    
+    }
     vector<string> generateParenthesis(int n) {
         string curr="";
-        solve(curr,n);
+        int open = 0;
+        int close = 0;
+        solve2(curr,n,open,close);
         return ans;
     }
 };
