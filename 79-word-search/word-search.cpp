9a4f8b6874291cebe6f5a4c5fc424c3373ffc47a
Prakhar Maheshwari <113543305+prakhar7017@users.noreply.github.com>
2026-09-13 01:03:47 +0530

    Time: 258 ms (51.84%) | Memory: 10.6 MB (64.73%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,35 +1,34 @@
 class Solution {
 public:
     vector<vector<int>>directions{{-1,0},{1,0},{0,-1},{0,1}};
-    bool isSafe(int idx,int x,int y,string& word,vector<vector<bool>>& vis,vector<vector<char>>& board){
-        if(x>=0 && x<board.size() && y>=0 && y<board[0].size() && !vis[x][y] && word[idx]==board[x][y]) return true;
+    bool isSafe(int idx,int x,int y,string& word,vector<vector<char>>& board){
+        if(x>=0 && x<board.size() && y>=0 && y<board[0].size() && board[x][y]!='$' && word[idx]==board[x][y]) return true;
         return false;
     }
-    bool solve(int idx,int currX,int currY,string&curr,string&word,vector<vector<bool>> &vis,vector<vector<char>>& board){
+    bool solve(int idx,int currX,int currY,string&curr,string&word,vector<vector<char>>& board){
         if(idx==word.size()-1) return true;
-        curr.push_back(board[currX][currY]);
-        vis[currX][currY]=true;
+
+        char temp = board[currX][currY];
+        board[currX][currY]='$';
 
         for(vector<int>&dir:directions){
             int newX = currX + dir[0];
             int newY = currY + dir[1];
 
-            if(isSafe(idx+1,newX,newY,word,vis,board)){
-                if(solve(idx+1,newX,newY,curr,word,vis,board)) return true;
+            if(isSafe(idx+1,newX,newY,word,board)){
+                if(solve(idx+1,newX,newY,curr,word,board)) return true;
             }
         }
-        vis[currX][currY]=false;
-        curr.pop_back();
+        board[currX][currY]=temp;
         return false;
     }
 
     bool exist(vector<vector<char>>& board, string word) {
         string curr = "";
-        vector<vector<bool>>vis(board.size(),vector<bool>(board[0].size(),false));
         for(int i=0;i<board.size();i++){
             for(int j=0;j<board[0].size();j++){
                 if (board[i][j]==word[0]){
-                    if(solve(0,i,j,curr,word,vis,board))return true;
+                    if(solve(0,i,j,curr,word,board)) return true;
                 }
             }
         }
