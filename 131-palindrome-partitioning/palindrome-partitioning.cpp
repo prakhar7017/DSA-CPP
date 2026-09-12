51bff92f9b9bd638fd4b877f9dbd98e1c2e99c6e
Prakhar Maheshwari <113543305+prakhar7017@users.noreply.github.com>
2026-09-13 01:45:14 +0530

    Time: 59 ms (35.51%) | Memory: 79.8 MB (34.65%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,45 +1,32 @@
 class Solution {
 public:
-    vector<vector<string>> partition(string s) {
-        int n = s.length();
-        vector<vector<bool>> dp(n, vector<bool>(n, false));
-
-        // Initialize the DP table for single characters and pairs
-        for (int i = 0; i < n; ++i) {
-            dp[i][i] = true;
+    vector<vector<string>> ans;
+    bool isPalindrome(int idx,int i,string&s){
+        int l =idx;
+        int r =i;
+        while(l<r){
+            if(s[l]!=s[r]) return false;
+            l++;
+            r--;
         }
-        for (int length = 2; length <= n; ++length) {
-            for (int i = 0; i <= n - length; ++i) {
-                int j = i + length - 1;
-                if (s[i] == s[j] && (length == 2 || dp[i + 1][j - 1])) {
-                    dp[i][j] = true;
-                }
-            }
-        }
-
-        vector<vector<string>> result;
-        vector<string> path;
-        backtrack(s, 0, path, result, dp);
-        return result;
+        return true;
     }
-
-private:
-    void backtrack(const string& s, int start, vector<string>& path, vector<vector<string>>& result, const vector<vector<bool>>& dp) {
-        // If we've reached the end of the string, add the current partition to the result list
-        if (start == s.length()) {
-            result.push_back(path);
+    void solve(int idx,vector<string>&curr,string&s){
+        if(idx==s.length()) {
+            ans.push_back(curr);
             return;
         }
-        // Explore all possible partitions
-        for (int end = start; end < s.length(); ++end) {
-            // Use the DP table to check if the substring s[start:end+1] is a palindrome
-            if (dp[start][end]) {
-                path.push_back(s.substr(start, end - start + 1));
-                // Recur to find other partitions
-                backtrack(s, end + 1, path, result, dp);
-                // Backtrack to explore other partitions
-                path.pop_back();
+        for(int i=idx;i<s.length();i++){
+            if(isPalindrome(idx,i,s)){
+                curr.push_back(s.substr(idx,i-idx+1));
+                solve(i+1,curr,s);
+                curr.pop_back();
             }
         }
+    }
+    vector<vector<string>> partition(string s) {
+        vector<string>curr;
+        solve(0,curr,s);
+        return ans;
     }
 };
