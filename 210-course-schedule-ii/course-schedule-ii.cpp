2d457433d655c0e1a8651c853d1654b3fa6b19f6
Prakhar Maheshwari <113543305+prakhar7017@users.noreply.github.com>
2026-09-20 16:47:23 +0530

    Time: 9 ms (17.25%) | Memory: 18.8 MB (37.89%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,34 +1,38 @@
 class Solution {
 public:
-    vector<int> topoSort(int numCourses, vector<vector<int>>& prerequisites){
-        vector<int>indegree(numCourses,0);
-        unordered_map<int,vector<int>>adj;
-        queue<int>q;
-        vector<int>ans;
-
-        for(vector<int>&preq:prerequisites){
-            adj[preq[1]].push_back(preq[0]);
-            indegree[preq[0]]++;
-        }
-
-        for(int i=0;i<numCourses;i++){
-            if(indegree[i]==0) q.push(i);
-        }
-
-        while(!q.empty()){
-            int currNode= q.front();
+    vector<int> BFS(int u, vector<int>& vis, vector<int>& indegree,
+                    unordered_map<int, vector<int>>& adj) {
+        queue<int> q;
+        for (int i = 0; i < indegree.size(); i++)
+            if (indegree[i] == 0)
+                q.push(i);
+        vector<int> ans;
+        while (!q.empty()) {
+            int curr = q.front();
             q.pop();
-            ans.push_back(currNode);
-
-            for(int &v:adj[currNode]){
+            ans.push_back(curr);
+            for (int& v : adj[curr]) {
                 indegree[v]--;
-                if(indegree[v]==0) q.push(v);
+                if (indegree[v] == 0) {
+                    vis[v] = true;
+                    q.push(v);
+                }
             }
         }
-
-        return ans.size()==numCourses ? ans: vector<int>();
+        return ans;
     }
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        return topoSort(numCourses,prerequisites);
+        unordered_map<int, vector<int>> adj;
+        vector<int> indegree(numCourses, 0);
+        vector<int> vis(numCourses, 0);
+        for (vector<int>& prerequisite : prerequisites) {
+            adj[prerequisite[1]].push_back(prerequisite[0]);
+            ;
+            indegree[prerequisite[0]]++;
+        }
+
+        vector<int> ans = BFS(0, vis, indegree, adj);
+        if(ans.size() == numCourses) return ans;
+        return {};
     }
 };
