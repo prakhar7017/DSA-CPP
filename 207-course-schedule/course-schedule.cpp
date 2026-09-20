b55b5f2a072b20bc8e9fbe6488fd94b67aa53086
Prakhar Maheshwari <113543305+prakhar7017@users.noreply.github.com>
2026-09-20 16:43:50 +0530

    Time: 7 ms (46.55%) | Memory: 20.4 MB (11.20%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        unordered_map<int, vector<int>> adj;
-        vector<int> indegree(numCourses, 0);
-        queue<int> q;
+    vector<int> BFS(int u, vector<int>&vis,vector<int>&indegree,unordered_map<int,vector<int>>&adj){
+        queue<int>q;
+        for(int i=0;i<indegree.size();i++) if(indegree[i]==0) q.push(i);
         vector<int>ans;
-        for (vector<int>& prerequisite : prerequisites) {
-            adj[prerequisite[1]].push_back(prerequisite[0]);
-            indegree[prerequisite[0]]++;
-        }
-
-        for (int i = 0; i < numCourses; i++) {
-            if (indegree[i] == 0)
-                q.push(i);
-        }
-
-        while (!q.empty()) {
-            int curr = q.front();
-            q.pop();
+        while(!q.empty()){
+            int curr = q.front();q.pop();
             ans.push_back(curr);
             for(int &v:adj[curr]){
                 indegree[v]--;
-                if(indegree[v]==0) q.push(v);
+                if(indegree[v]==0){
+                    vis[v]=true;
+                    q.push(v);
+                }
             }
         }
+        return ans;
+    }
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        unordered_map<int,vector<int>>adj;
+        vector<int>indegree(numCourses,0);
+        vector<int>vis(numCourses,0);
+        for(vector<int>&prerequisite:prerequisites){
+            adj[prerequisite[1]].push_back(prerequisite[0]);;
+            indegree[prerequisite[0]]++;
+        }
 
+        vector<int> ans = BFS(0,vis,indegree,adj);
         return ans.size()==numCourses;
-
-
     }
 };
