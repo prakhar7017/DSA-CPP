20a869e7b78f13fac38a13288f7037c9c563bcd9
Prakhar Maheshwari <113543305+prakhar7017@users.noreply.github.com>
2026-09-20 16:22:47 +0530

    Time: 0 ms (100.00%) | Memory: 20.7 MB (27.69%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,47 +1,29 @@
 class Solution {
 public:
-    vector<int>parent;
-    vector<int>rank;
-    int components;
-
-    int findParent(int x){
-        if(parent[x]==x) return x;
-        return parent[x]=findParent(parent[x]);
-    }
-
-    void Union(int x,int y){
-        x = findParent(x);
-        y = findParent(y);
-
-        if(x==y) return;
-
-        if(rank[x]<rank[y]){
-            parent[x]=y;
-        }else if(rank[x]>rank[y]){
-            parent[y]=x;
-        }else{
-            parent[y]=x;
-            rank[x]++;
+    void DFS(int u,vector<int>&vis,unordered_map<int,vector<int>>&adj){
+        vis[u]=true;
+        for(int &v:adj[u]){
+            if(!vis[v]) DFS(v,vis,adj);
         }
-        components--;
     }
-
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int n = isConnected.size();
-        components = n;
-
-        parent.resize(n);
-        rank.resize(n,0);
-
-        iota(begin(parent),end(parent),0);
-
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
+        unordered_map<int,vector<int>>adj;
+        for(int i=0;i<isConnected.size();i++){
+            for(int j=0;j<isConnected[0].size();j++){
                 if(isConnected[i][j]==1){
-                    Union(i,j);
+                    adj[i].push_back(j);
                 }
             }
         }
-        return components;
+
+        vector<int>vis(isConnected.size(),0);
+        int count=0;
+        for(int i=0;i<isConnected.size();i++){
+            if(!vis[i]){
+                DFS(i,vis,adj);
+                count++;
+            }
+        }
+        return count;
     }
 };
